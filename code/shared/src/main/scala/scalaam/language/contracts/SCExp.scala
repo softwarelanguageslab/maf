5f6b0286ca2baa0b4b7def9f7716cf53a9a72dbc
Bram Vandenbogaerde <contact@bramvdb.com>
2020-09-02 15:16:20 +0200

    Added simple contract language

---
+++ code/shared/src/main/scala/scalaam/language/contracts/SCExp.scala
@@ -0,0 +1,219 @@
+package scalaam.language.contracts
+
+import scalaam.core.{Expression, Identifier, Identity, Label}
+import scalaam.language.sexp.Value
+
+case object FLAT_CONTRACT         extends Label
+case object HIGHER_ORDER_CONTRACT extends Label
+case object DEPENDENT_CONTRACT    extends Label
+case object MONITOR               extends Label
+case object VALUE                 extends Label
+case object BLAME                 extends Label
+case object IDENTIFIER            extends Label
+case object LAMBDA                extends Label
+case object FUNCTION_AP           extends Label
+case object RAISE                 extends Label
+case object CHECK                 extends Label
+case object LETREC                extends Label
+case object BEGIN                 extends Label
+case object IF                    extends Label
+case object SET                   extends Label
+
+/**
+  * A language for defining software contracts
+  */
+trait ScExp extends Expression {
+  implicit class FreeVariablesList(list: List[Expression]) {
+    def fv: Set[String] = list.foldLeft(Set[String]())((a, b) => a ++ b.fv)
+  }
+}
+trait ScContract extends ScExp
+case class ScFlatContract(expression: ScExp, idn: Identity) extends ScContract {
+
+  /** The set of free variables appearing in this expression. */
+  override def fv: Set[String] = expression.fv
+
+  /** A label indicating the type of an expression. */
+  override def label: Label = FLAT_CONTRACT
+
+  /** Returns the list of subexpressions of the given expression. */
+  override def subexpressions: List[Expression] = List(expression)
+}
+
+case class ScHigherOrderContract(domain: ScContract, range: ScContract, idn: Identity)
+    extends ScContract {
+
+  /** The set of free variables appearing in this expression. */
+  override def fv: Set[String] = domain.fv ++ range.fv
+
+  /** A label indicating the type of an expression. */
+  override def label: Label = HIGHER_ORDER_CONTRACT
+
+  /** Returns the list of subexpressions of the given expression. */
+  override def subexpressions: List[Expression] = List(domain, range)
+}
+
+case class ScDependentContract(domain: ScContract, rangeMaker: ScLambda, idn: Identity)
+    extends ScContract {
+
+  /** The set of free variables appearing in this expression. */
+  override def fv: Set[String] = domain.fv ++ rangeMaker.fv
+
+  /** A label indicating the type of an expression. */
+  override def label: Label = DEPENDENT_CONTRACT
+
+  /** Returns the list of subexpressions of the given expression. */
+  override def subexpressions: List[Expression] = List(domain, rangeMaker)
+}
+
+case class ScIdentifier(name: String, idn: Identity) extends ScExp {
+
+  /** The set of free variables appearing in this expression. */
+  override def fv: Set[String] = Set(name)
+
+  /** A label indicating the type of an expression. */
+  override def label: Label = IDENTIFIER
+
+  /** Returns the list of subexpressions of the given expression. */
+  override def subexpressions: List[Expression] = List()
+}
+
+trait ScLiterals extends ScExp
+
+case class ScLambda(variables: List[ScIdentifier], body: Expression, idn: Identity)
+    extends ScLiterals
+    with ScExp {
+
+  /** The set of free variables appearing in this expression. */
+  override def fv: Set[String] =
+    body.fv -- variables.fv
+
+  /** A label indicating the type of an expression. */
+  override def label: Label = LAMBDA
+
+  /** Returns the list of subexpressions of the given expression. */
+  override def subexpressions: List[Expression] = List(body)
+}
+
+case class ScLetRec(name: ScIdentifier, binding: ScExp, body: ScExp, idn: Identity) extends ScExp {
+
+  /** The set of free variables appearing in this expression. */
+  override def fv: Set[String] = (body.fv ++ binding.fv) - name.name
+
+  /** A label indicating the type of an expression. */
+  override def label: Label = LETREC
+
+  /** Returns the list of subexpressions of the given expression. */
+  override def subexpressions: List[Expression] = List(body, binding)
+}
+
+case class ScValue(value: Value, idn: Identity) extends ScLiterals {
+
+  /** The set of free variables appearing in this expression. */
+  override def fv: Set[String] = Set()
+
+  /** A label indicating the type of an expression. */
+  override def label: Label = VALUE
+
+  /** Returns the list of subexpressions of the given expression. */
+  override def subexpressions: List[Expression] = List()
+}
+
+case class ScFunctionAp(operator: Expression, operands: List[Expression], idn: Identity)
+    extends ScExp {
+
+  /** The set of free variables appearing in this expression. */
+  override def fv: Set[String] = operator.fv ++ operands.fv
+
+  /** A label indicating the type of an expression. */
+  override def label: Label = FUNCTION_AP
+
+  /** Returns the list of subexpressions of the given expression. */
+  override def subexpressions: List[Expression] = List(operator) ++ operands
+}
+
+case class ScBegin(expressions: List[Expression], idn: Identity) extends ScExp {
+
+  /** The set of free variables appearing in this expression. */
+  override def fv: Set[String] = expressions.fv
+
+  /** A label indicating the type of an expression. */
+  override def label: Label = BEGIN
+
+  /** Returns the list of subexpressions of the given expression. */
+  override def subexpressions: List[Expression] = expressions
+}
+
+case class ScSet(variable: ScIdentifier, value: ScExp, idn: Identity) extends ScExp {
+
+  /** The set of free variables appearing in this expression. */
+  override def fv: Set[String] = value.fv
+
+  /** A label indicating the type of an expression. */
+  override def label: Label = SET
+
+  /** Returns the list of subexpressions of the given expression. */
+  override def subexpressions: List[Expression] = List(value)
+}
+
+case class ScMon(contract: ScContract, expression: ScExp, idn: Identity) extends ScExp {
+
+  /** The set of free variables appearing in this expression. */
+  override def fv: Set[String] = contract.fv ++ expression.fv
+
+  /** A label indicating the type of an expression. */
+  override def label: Label = MONITOR
+
+  /** Returns the list of subexpressions of the given expression. */
+  override def subexpressions: List[Expression] = List(contract, expression)
+}
+
+case class ScCheck(blame: Identity, expression: ScExp, returnValue: ScExp, idn: Identity)
+    extends ScExp {
+
+  /** The set of free variables appearing in this expression. */
+  override def fv: Set[String] = expression.fv ++ returnValue.fv
+
+  /** A label indicating the type of an expression. */
+  override def label: Label = CHECK
+
+  /** Returns the list of subexpressions of the given expression. */
+  override def subexpressions: List[Expression] = List(expression, returnValue)
+}
+
+case class ScIf(condition: ScExp, consequent: ScExp, alternative: ScExp, idn: Identity)
+    extends ScExp {
+
+  /** The set of free variables appearing in this expression. */
+  override def fv: Set[String] = condition.fv ++ consequent.fv ++ alternative.fv
+
+  /** A label indicating the type of an expression. */
+  override def label: Label = IF
+
+  /** Returns the list of subexpressions of the given expression. */
+  override def subexpressions: List[Expression] = List(condition, consequent, alternative)
+}
+
+case class ScRaise(error: String, idn: Identity) extends ScExp {
+
+  /** The set of free variables appearing in this expression. */
+  override def fv: Set[String] = Set()
+
+  /** A label indicating the type of an expression. */
+  override def label: Label = RAISE
+
+  /** Returns the list of subexpressions of the given expression. */
+  override def subexpressions: List[Expression] = List()
+}
+
+case class ScBlame(blame: Identity, idn: Identity) extends ScExp {
+
+  /** The set of free variables appearing in this expression. */
+  override def fv: Set[String] = Set()
+
+  /** A label indicating the type of an expression. */
+  override def label: Label = BLAME
+
+  /** Returns the list of subexpressions of the given expression. */
+  override def subexpressions: List[Expression] = List()
+}
---
+++ code/shared/src/main/scala/scalaam/language/contracts/SCExpCompiler.scala
@@ -0,0 +1,91 @@
+package scalaam.language.contracts
+
+import scalaam.core.{Identifier, Identity}
+import scalaam.language.sexp.{SExp, SExpId, SExpPair, SExpValue, ValueNil, ValueString}
+
+/**
+  * Compiles a program of S-expressions into a program of ScExp
+  */
+object SCExpCompiler {
+  object :: {
+    def unapply(value: SExp): Option[(SExp, SExp)] = {
+      value match {
+        case SExpPair(car, cdr, _) => Some((car, cdr))
+        case _                     => None
+      }
+    }
+  }
+
+  object Ident {
+    def unapply(value: SExpId): Option[(String)] = Some(value.id.name)
+  }
+
+  object IdentWithIdentity {
+    def unapply(arg: SExpId): Option[(String, Identity)] =
+      Some((arg.id.name, arg.idn))
+  }
+
+  object ListNil {
+    def unapply(value: SExp): Option[(Identity)] = value match {
+      case SExpValue(ValueNil, idn) => Some((idn))
+      case _                        => None
+    }
+  }
+
+  case class SCExpCompilerException(message: String) extends Exception
+
+  def compile_params(s: SExp): List[ScIdentifier] = s match {
+    case SExpPair(IdentWithIdentity(name, idn), cdr, _) =>
+      ScIdentifier(name, idn) :: compile_params(cdr)
+    case SExpValue(ValueNil, _) => List()
+  }
+
+  def compile_sequence(s: SExp): List[ScExp] = s match gi{
+    case SExpPair(exp, cdr, _)  => compile(exp) :: compile_sequence(cdr)
+    case SExpValue(ValueNil, _) => List()
+  }
+
+  def compile(prog: SExp): ScExp = prog match {
+    case Ident("set") :: IdentWithIdentity(name, idn) :: exp :: SExpValue(ValueNil, _) =>
+      ScSet(ScIdentifier(name, idn), compile(exp), prog.idn)
+
+    case Ident("flat") :: expr :: ListNil(_) =>
+      ScFlatContract(compile(expr), prog.idn)
+
+    case Ident("->") :: domain :: range :: ListNil(_) =>
+      val domainCompiled = compile(domain).asInstanceOf[ScContract]
+      val rangeCompiled  = compile(range).asInstanceOf[ScContract]
+      ScHigherOrderContract(domainCompiled, rangeCompiled, prog.idn)
+
+    case SExpId(identifier) => ScIdentifier(identifier.name, prog.idn)
+    case Ident("lambda") :: params :: expression :: ListNil(_) =>
+      val compiledParams     = compile_params(params)
+      val compiledExpression = compile(expression)
+      ScLambda(compiledParams, compiledExpression, prog.idn)
+
+    case Ident("letrec") :: (IdentWithIdentity(name, idn) :: bindingExpression :: ListNil(_)) :: expression :: ListNil(
+          _
+        ) =>
+      val compiledBindingExpression = compile(bindingExpression)
+      val compiledExpression        = compile(expression)
+      ScLetRec(ScIdentifier(name, idn), compiledBindingExpression, compiledExpression, prog.idn)
+
+    case Ident("mon") :: contract :: expression :: ListNil(_) =>
+      val compiledContract   = compile(contract).asInstanceOf[ScContract]
+      val compiledExpression = compile(expression)
+      ScMon(compiledContract, compiledExpression, prog.idn)
+
+    case Ident("if") :: condition :: consequent :: alternative :: ListNil(_) =>
+      val compiledCondition   = compile(condition)
+      val compiledConsequent  = compile(consequent)
+      val compiledAlternative = compile(alternative)
+      ScIf(compiledCondition, compiledConsequent, compiledAlternative, prog.idn)
+    case Ident("raise") :: SExpValue(ValueString(str), _) =>
+      ScRaise(str, prog.idn)
+
+    case Ident("begin") :: expressions =>
+      ScBegin(compile_sequence(expressions), prog.idn)
+
+    case SExpValue(value, _) => ScValue(value, prog.idn)
+  }
+}
