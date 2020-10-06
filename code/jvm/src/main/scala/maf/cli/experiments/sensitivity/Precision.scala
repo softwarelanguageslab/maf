809ab26a21548981a11ed103d15616f1ed9a236a
Quentin Stiévenart <quentin.stievenart@gmail.com>
2020-10-06 16:52:09 +0200

    Forgot the actual evaluation code

---
+++ code/jvm/src/main/scala/maf/cli/experiments/sensitivity/Precision.scala
@@ -0,0 +1,79 @@
+package maf.cli.experiments.sensitivity
+
+import maf.bench.scheme.SchemeBenchmarkPrograms
+import maf.language.scheme._
+import maf.language.scheme.primitives._
+import maf.lattice._
+import maf.modular._
+import maf.modular.scheme._
+import maf.modular.scheme.modf._
+import maf.util._
+import maf.util.benchmarks.Timeout
+
+import maf.cli.experiments.precision._
+
+import scala.concurrent.duration._
+
+object PrecisionComparison extends AnalysisComparison[
+    ConstantPropagation.I,
+    ConstantPropagation.R,
+    ConstantPropagation.B,
+    ConstantPropagation.C,
+    ConstantPropagation.S,
+    Concrete.Sym
+] {
+  val benchmarks = {
+    List("test/R5RS/gabriel.scm") ++
+    SchemeBenchmarkPrograms.gabriel
+  }
+
+  var path: String = "<none>"
+
+  // The base analysis
+  def baseAnalysis(prg: SchemeExp): Analysis =
+    new SimpleSchemeModFAnalysis(prg)
+        with SchemeModFNoSensitivity
+        with SchemeConstantPropagationDomain
+        with LIFOWorklistAlgorithm[SchemeExp]
+
+  // The analysis with improved precision
+  def improvedAnalysis(prg: SchemeExp): Analysis =
+    new SimpleSchemeModFAnalysis(prg)
+        with SchemeModFUserGuidedSensitivity1
+        with SchemeConstantPropagationDomain
+        with LIFOWorklistAlgorithm[SchemeExp] {
+      override def toString() = "Improved"
+  }
+
+  def otherAnalyses() = List((improvedAnalysis, "Improved"))
+
+  override def analysisTimeout() = Timeout.start(Duration(2, MINUTES)) // timeout for (non-base) analyses
+  override def concreteTimeout() = Timeout.start(Duration(2, MINUTES))
+  override def concreteRuns() = 2
+
+  def main(args: Array[String]) = runBenchmarks() // check("test/primtest.scm")
+
+  def check(benchmark: Benchmark) = {
+      val txt = Reader.loadFile(benchmark)
+      val prg = SchemeParser.parse(txt)
+      val con = runInterpreter(prg, path).get
+      val abs = runAnalysis(baseAnalysis,"base analysis",prg,path).get
+      val allKeys = con.keys ++ abs.keys
+      val interestingKeys = allKeys.filter(_.isInstanceOf[RetAddr])
+      interestingKeys.foreach { k =>
+          println(s"$k -> ${abs.getOrElse(k,"⊥")} ; ${con.getOrElse(k,"⊥")} ")
+      }
+  }
+
+  def runBenchmarks() = {
+    benchmarks.foreach(b => {
+      System.gc()
+      path = b
+      runBenchmark(b)
+    })
+    println("Results:")
+    val columns = List("Improved")
+    println(results.withDefaultValue(None).prettyString(columns = columns, format = _.map(_.toString()).getOrElse("TIMEOUT")))
+    println(results.withDefaultValue(None).toLatexString(columns = columns, format = _.map(_.toString()).getOrElse("--")))
+  }
+}
