54b707246a40d88296457f9c475643c366c78e0d
Jens Van der Plas <jevdplas@vub.be>
2020-12-02 09:08:15 +0100

    Use Store Opt version for web.

--- code/js/src/main/scala/maf/web/Main.scala
+++ code/js/src/main/scala/maf/web/Main.scala
@@ -1,5 +1,6 @@
 package maf.web
 
+import maf.core.Identity
 import maf.language.scheme._
 import maf.modular._
 import maf.modular.incremental.scheme.SchemeAnalyses._
@@ -44,12 +45,12 @@ object Main {
     body.appendChild(input)
   }
 
-  def standardAnalysis(program: SchemeExp) = new SimpleSchemeModFAnalysis(program) with SchemeModFNoSensitivity
+  def standardAnalysis(program: SchemeExp): SimpleSchemeModFAnalysis with SchemeModFNoSensitivity with SchemeConstantPropagationDomain with DependencyTracking[SchemeExp] with FIFOWorklistAlgorithm[SchemeExp] = new SimpleSchemeModFAnalysis(program) with SchemeModFNoSensitivity
     with SchemeConstantPropagationDomain
     with DependencyTracking[SchemeExp]
     with FIFOWorklistAlgorithm[SchemeExp] {
     //override def allocCtx(nam: Option[String], clo: lattice.Closure, args: List[Value], call: Position, caller: Component) = super.allocCtx(nam,clo,args,call,caller)
-    def key(cmp: Component) = expr(cmp).idn
+    def key(cmp: Component): Identity = expr(cmp).idn
     override def step(t: Timeout.T) = {
       val cmp = workList.head
       println(cmp)
@@ -59,16 +60,24 @@ object Main {
       new IntraAnalysis(cmp) with BigStepModFIntra with DependencyTrackingIntra
   }
 
-  def incrementalReanalysis(program: SchemeExp) = new IncrementalSchemeModFCPAnalysis(program) with DependencyTracking[SchemeExp] {
-    override def intraAnalysis(cmp: SchemeModFComponent) = new IntraAnalysis(cmp) with IncrementalSchemeModFBigStepIntra with DependencyTrackingIntra
-    analyze(Timeout.start(Duration(5, MINUTES)))
+  def incrementalReanalysis(program: SchemeExp): IncrementalSchemeModFCPAnalysisStoreOpt with DependencyTracking[SchemeExp] = new IncrementalSchemeModFCPAnalysisStoreOpt(program) with DependencyTracking[SchemeExp] {
+    override def intraAnalysis(cmp: SchemeModFComponent) = new IntraAnalysis(cmp) with IncrementalSchemeModFBigStepIntra with IncrementalReturnValueIntraAnalysis with DependencyTrackingIntra
+    println("Starting initial analysis.") // Will be logged to console.
+    try {
+      analyze(Timeout.start(Duration(5, MINUTES)))
+    } catch {
+      case t: Throwable => System.err.println(t.getMessage) // Will display an error in the console.
+        throw t
+    }
+    println("Finished initial analysis. Preparing for reanalysis.")
     version = New
     optimisationFlag = true
     findUpdatedExpressions(program).flatMap(mapping).foreach(addToWorkList)
+    println("Preparation finished. Starting reanalysis.")
   }
 
   // Select the analysis to be used by the web visualisation.
-  val analysis: SchemeExp => ModAnalysis[_] with SequentialWorklistAlgorithm[_] with DependencyTracking[_] = standardAnalysis
+  val analysis: SchemeExp => ModAnalysis[_] with SequentialWorklistAlgorithm[_] with DependencyTracking[_] = incrementalReanalysis
 
   def loadFile(text: String): Unit = {
     val program = SchemeParser.parse(text)
