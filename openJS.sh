17b180f81021626b32eb038e48149376b16567f4
Jens Van der Plas <jevdplas@vub.be>
2020-12-10 13:32:11 +0100

    Bump dependency version numbers.

--- openJS.sh
+++ openJS.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 # sbt fastOptJS # Can be used separately (this is faster in IntelliJ since it keeps sbt running where it should otherwise restart every time this script is run).
+# sbt fullOptJS # Full optimisation for in-production code.
 open -a safari ./maf.html
