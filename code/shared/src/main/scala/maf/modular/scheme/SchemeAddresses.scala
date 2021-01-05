29579113668f6df0fb137e8de8d050117964fed1
Noah Van Es <noahvanes@me.com>
2021-01-05 16:22:28 +0100

    indentation

--- code/shared/src/main/scala/maf/modular/scheme/SchemeAddresses.scala
+++ code/shared/src/main/scala/maf/modular/scheme/SchemeAddresses.scala
@@ -8,6 +8,18 @@ import maf.language.scheme._
  * @tparam Context A type of Contexts that can be used to distinguish more addresses. The type context is covariant.
  */
 trait SchemeAddr[+Context] extends Address
-case class VarAddr[Context](id: Identifier, ctx: Context)   extends SchemeAddr[Context] { def printable = true;  def idn: Identity =  id.idn;       override def toString: String = s"var ($id [${id.idn.pos}])"        }
-case class PtrAddr[Context](exp: SchemeExp, ctx: Context)   extends SchemeAddr[Context] { def printable = false; def idn: Identity =  exp.idn;      override def toString: String = s"ptr (${exp.idn})" }
-case class PrmAddr(nam: String)                             extends SchemeAddr[Nothing] { def printable = false; def idn: Identity = Identity.none; override def toString: String = s"prm ($nam)"       }
+case class VarAddr[Context](id: Identifier, ctx: Context) extends SchemeAddr[Context] { 
+    def printable = true  
+    def idn: Identity = id.idn       
+    override def toString: String = s"var ($id [${id.idn.pos}])"        
+}
+case class PtrAddr[Context](exp: SchemeExp, ctx: Context) extends SchemeAddr[Context] { 
+    def printable = false
+    def idn: Identity = exp.idn
+    override def toString: String = s"ptr (${exp.idn})" 
+}
+case class PrmAddr(nam: String) extends SchemeAddr[Nothing] { 
+    def printable = false
+    def idn: Identity = Identity.none
+    override def toString: String = s"prm ($nam)"       
+}
