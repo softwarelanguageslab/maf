e69fb79ba68a910eb831b2ad71c36c0217b5c9bb
Quentin Stiévenart <quentin.stievenart@gmail.com>
2021-01-07 13:30:43 +0100

    Change fromFolder/toFolder interface for better formatting

--- code/shared/src/main/scala/maf/bench/scheme/IncrementalSchemeBenchmarkPrograms.scala
+++ code/shared/src/main/scala/maf/bench/scheme/IncrementalSchemeBenchmarkPrograms.scala
@@ -1,21 +1,19 @@
 package maf.bench.scheme
 
 object IncrementalSchemeBenchmarkPrograms {
-  lazy val threads: Set[String] = SchemeBenchmarkPrograms.fromFolder("test/changes/cscheme/threads",
-                                                                     "puzzle.scm", // Needs call-with-current-continuation.
-                                                                     ".DS_Store"
+  lazy val threads: Set[String] = SchemeBenchmarkPrograms.fromFolder("test/changes/cscheme/threads")(
+    "puzzle.scm", // Needs call-with-current-continuation.
+    ".DS_Store"
   )
   lazy val concurrent: Set[String] = threads
-  lazy val sequential: Set[String] = SchemeBenchmarkPrograms.fromFolder(
-    "test/changes/scheme",
+  lazy val sequential: Set[String] = SchemeBenchmarkPrograms.fromFolder("test/changes/scheme")(
     "qeval.scm", // define-syntax, force, delay
     "scheme.scm", // error in program
     "machine-simulator.scm", // map with three arguments
     ".DS_Store"
   )
 
-  lazy val scam2020ModF: Set[String] = SchemeBenchmarkPrograms.toFolder(
-    "test/changes/scheme",
+  lazy val scam2020ModF: Set[String] = SchemeBenchmarkPrograms.toFolder("test/changes/scheme")(
     "leval.scm",
     "machine-simulator.scm",
     "mceval-dynamic.scm",
@@ -25,15 +23,15 @@ object IncrementalSchemeBenchmarkPrograms {
     "peval.scm"
   )
 
-  lazy val scam2020ModConc: Set[String] = SchemeBenchmarkPrograms.toFolder("test/changes/cscheme/threads",
-                                                                           "actors.scm",
-                                                                           "crypt.scm",
-                                                                           "crypt2.scm",
-                                                                           "msort.scm",
-                                                                           "pc.scm",
-                                                                           "pps.scm",
-                                                                           "stm.scm",
-                                                                           "sudoku.scm",
-                                                                           "mcarlo2.scm"
+  lazy val scam2020ModConc: Set[String] = SchemeBenchmarkPrograms.toFolder("test/changes/cscheme/threads")(
+    "actors.scm",
+    "crypt.scm",
+    "crypt2.scm",
+    "msort.scm",
+    "pc.scm",
+    "pps.scm",
+    "stm.scm",
+    "sudoku.scm",
+    "mcarlo2.scm"
   )
 }
