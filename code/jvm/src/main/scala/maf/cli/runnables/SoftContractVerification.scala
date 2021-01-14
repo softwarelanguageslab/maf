3e78ea749864092852ab2136b559c6f6d8668ea3
bramvdbogaerde <contact@bramvdb.com>
2021-01-14 15:20:09 +0100

    added REPL for interactive testing

--- code/jvm/src/main/scala/maf/cli/runnables/SoftContractVerification.scala
+++ code/jvm/src/main/scala/maf/cli/runnables/SoftContractVerification.scala
@@ -1,10 +1,12 @@
 package maf.cli.runnables
 
 import maf.cli.experiments.contracts.{Benchmarks, NguyenBenchmarks}
+import maf.cli.experiments.contracts.MafBenchmarks
 
 object SoftContractVerification {
   def main(args: Array[String]): Unit = {
-    val benchmarks: List[Benchmarks] = List(NguyenBenchmarks())
+    //val benchmarks: List[Benchmarks] = List(NguyenBenchmarks(), MafBenchmarks())
+    val benchmarks = List(MafBenchmarks())
     benchmarks.foreach(_.run())
   }
 }
