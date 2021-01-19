579d078a964cd598fa394e5841e48955ca7c0243
Jens Van der Plas <jevdplas@vub.be>
2021-01-19 14:19:12 +0100

    Scheme tests for string-fill!

--- code/shared/src/main/scala/maf/language/sexp/SExpParser.scala
+++ code/shared/src/main/scala/maf/language/sexp/SExpParser.scala
@@ -2,7 +2,7 @@ package maf.language.sexp
 
 import maf.core.Position._
 import maf.core._
-import maf.lattice.NumOps
+import maf.lattice._
 
 /**
  * Implementation of a simple s-expression parser, which supports some
