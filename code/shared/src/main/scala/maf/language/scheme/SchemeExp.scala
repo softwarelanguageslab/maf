04f8afe791e38d27098e34b716ad73566ba6b782
Quentin Stiévenart <quentin.stievenart@gmail.com>
2021-01-29 14:44:55 +0100

    Move CSchemeExp to SchemeExp.scala before sealing

--- code/shared/src/main/scala/maf/language/scheme/SchemeExp.scala
+++ code/shared/src/main/scala/maf/language/scheme/SchemeExp.scala
@@ -619,4 +619,26 @@ case class SchemeCodeChange(
     nw: SchemeExp,
     idn: Identity)
     extends ChangeExp[SchemeExp]
-       with SchemeExp
+    with SchemeExp
+
+
+trait CSchemeExp extends SchemeExp
+
+case object FRK extends Label // Fork
+case object JOI extends Label // Join
+
+/** Fork a thread with an expression to evaluate. */
+case class CSchemeFork(body: SchemeExp, idn: Identity) extends CSchemeExp {
+  def fv: Set[String] = body.fv
+  def label: Label = FRK
+  def subexpressions: List[Expression] = List(body)
+  override val height: Int = body.height + 1
+}
+
+/** Join a thread, given an expression that should evaluate to a TID. */
+case class CSchemeJoin(tExp: SchemeExp, idn: Identity) extends CSchemeExp {
+  def fv: Set[String] = tExp.fv
+  def label: Label = JOI
+  def subexpressions: List[Expression] = List(tExp)
+  override val height: Int = tExp.height + 1
+}
