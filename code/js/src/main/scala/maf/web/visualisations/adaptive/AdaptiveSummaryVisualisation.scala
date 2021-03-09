727410254dd9bd047325b966b29130fe337ecec5
Noah Van Es <noahvanes@me.com>
2021-03-09 22:10:28 +0100

    proper navigation between bar charts in adaptive webvis

--- code/js/src/main/scala/maf/web/visualisations/adaptive/AdaptiveSummaryVisualisation.scala
+++ code/js/src/main/scala/maf/web/visualisations/adaptive/AdaptiveSummaryVisualisation.scala
@@ -46,16 +46,29 @@ class AdaptiveSummaryVisualisation(
     def node: dom.Node
     def refresh(): Unit
   }
+  sealed trait ChildView extends View {
+    def parent: View
+    def innerNode: dom.Node
+    lazy val node = {
+      val div = document.createElement("div")
+      val backButton = document.createElement("button").asInstanceOf[html.Button]
+      backButton.onclick = (_: dom.raw.MouseEvent) => switchView(parent)
+      backButton.innerHTML = "Go back"
+      div.appendChild(backButton) 
+      div.appendChild(this.innerNode)
+      div
+    }
+  }
   case object ModuleView extends View {
     def node = ModuleBarChart.node
     def refresh() = ModuleBarChart.loadDataSorted(analysis.summary.content)
   }
-  case class ComponentView(ms: analysis.ModuleSummary) extends View {
-    def node = ComponentBarChart.node
+  case class ComponentView(ms: analysis.ModuleSummary, parent: View) extends ChildView {
+    def innerNode = ComponentBarChart.node
     def refresh() = ComponentBarChart.loadDataSorted(ms.content)
   }
-  case class DependencyView(ms: MultiSet[Dependency]) extends View {
-    def node = DependencyBarChart.node
+  case class DependencyView(ms: MultiSet[Dependency], parent: View) extends ChildView {
+    def innerNode = DependencyBarChart.node
     def refresh() = DependencyBarChart.loadDataSorted(ms.content)
   }
 
@@ -70,14 +83,14 @@ class AdaptiveSummaryVisualisation(
     type Data = (analysis.SchemeModule, analysis.ModuleSummary)
     def key(d: Data): String = d._1.toString
     def value(d: Data): Int = d._2.cost
-    override def onClick(d: Data) = switchView(ComponentView(d._2))
+    override def onClick(d: Data) = switchView(ComponentView(d._2, currentView))
   }
 
   object ComponentBarChart extends BarChart(width, height) {
     type Data = (analysis.Component, MultiSet[Dependency])
     def key(d: Data): String = d._1.toString
     def value(d: Data): Int = d._2.cardinality
-    override def onClick(d: Data) = switchView(DependencyView(d._2))
+    override def onClick(d: Data) = switchView(DependencyView(d._2, currentView))
   }
 
   object DependencyBarChart extends BarChart(width, height) {
@@ -95,7 +108,9 @@ class AdaptiveSummaryVisualisation(
   currentView.refresh()
 
   def switchView(view: View) {
+    // remove previous view
     node.removeChild(currentView.node)
+    // setup new view  
     currentView = view
     node.appendChild(currentView.node)
     currentView.refresh()
