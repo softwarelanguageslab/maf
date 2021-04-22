eecc4ccf395699f4356e263bf78043ad728481ae
Jens Van der Plas <jevdplas@vub.be>
2021-04-22 13:12:49 +0200

    Simplify AddressTaintLattice

--- code/shared/src/main/scala/maf/lattice/instances/taint/AddressTaintLattice.scala
+++ code/shared/src/main/scala/maf/lattice/instances/taint/AddressTaintLattice.scala
@@ -10,25 +10,22 @@ import maf.lattice.interfaces.taint.TaintLattice
  * 
  *    {a1, a2}         ...
  *     /    \     
- *   {a1}  {a2} {a3}  {a4}  ... 
- *     \\    \    |    /    //
+ *   {a1} {a2} {a3}  {a4}  ... 
+ *     \    \    |    /    /
  *               {}
  */
 // format: on
 object AddressTaintLattice {
 
   type TaintSet = Set[Address]
-  sealed trait T[Taint]
-  case class Taints(taints: TaintSet) extends T[TaintSet]
+  case class T[Taintset](taints: TaintSet) // We omit a separate trait so that we don't have to match in the definitions of join, subsumes and eql.
 
   class AddressTaintLattice extends TaintLattice[TaintSet, T] {
-    override def inject(t: TaintSet): T[TaintSet] = Taints(t)
-    override def bottom: T[TaintSet] = Taints(Set())
-    override def join(x: T[TaintSet], y: => T[TaintSet]): T[TaintSet] = (x, y) match { case (Taints(x), Taints(y)) => Taints(x.union(y)) }
-    override def subsumes(x: T[TaintSet], y: => T[TaintSet]): Boolean = (x, y) match { case (Taints(x), Taints(y)) => y.subsetOf(x) }
-    override def eql[B: BoolLattice](x: T[TaintSet], y: T[TaintSet]): B = (x, y) match {
-      case (Taints(x), Taints(y)) => BoolLattice[B].inject(x == y)
-    }
+    override def inject(t: TaintSet): T[TaintSet] = T(t)
+    override def bottom: T[TaintSet] = T(Set())
+    override def join(x: T[TaintSet], y: => T[TaintSet]): T[TaintSet] = T(x.taints.union(y.taints))
+    override def subsumes(x: T[TaintSet], y: => T[TaintSet]): Boolean = y.taints.subsetOf(x.taints)
+    def eql[B: BoolLattice](x: T[TaintSet], y: T[TaintSet]): B = BoolLattice[B].inject(x.taints == y.taints)
     override def show(v: T[TaintSet]): String = v.toString
   }
 }
