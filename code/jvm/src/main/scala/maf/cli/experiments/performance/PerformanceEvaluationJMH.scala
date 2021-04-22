fcc67af289fc56d428d68c5e86ed01e3ece5a762
Quentin Stiévenart <quentin.stievenart@gmail.com>
2021-04-22 10:47:04 +0200

    Setup jmh for performance evaluation (to run: maf/jmh:run)

---
+++ code/jvm/src/main/scala/maf/cli/experiments/performance/PerformanceEvaluationJMH.scala
@@ -0,0 +1,45 @@
+package maf.cli.experiments.performance
+
+import maf.language.scheme._
+import maf.language.CScheme._
+import maf.modular.ModAnalysis
+import maf.util._
+import maf.cli.experiments.SchemeAnalyses
+
+import java.util.concurrent.TimeUnit
+import org.openjdk.jmh.annotations.{Benchmark, BenchmarkMode, Mode, OutputTimeUnit, Setup}
+
+trait PerformanceEvaluationJMH {
+  def runProgram(file: String, analysis: SchemeExp => ModAnalysis[SchemeExp]): Unit = {
+    val program = CSchemeParser.parse(Reader.loadFile(s"../../$file"))
+    val _ = analysis(program).analyze()
+  }
+}
+
+@OutputTimeUnit(TimeUnit.MILLISECONDS)
+@BenchmarkMode(Array(Mode.SingleShotTime))
+class PerformanceEvaluationJMH0CFA extends PerformanceEvaluationJMH {
+  def analysis = SchemeAnalyses.kCFAAnalysis(_, 0)
+
+  @Benchmark
+  def mceval: Unit = runProgram("test/R5RS/various/mceval.scm", analysis)
+  @Benchmark
+  def leval: Unit = runProgram("test/R5RS/icp/icp_3_leval.scm", analysis)
+  @Benchmark
+  def sboyer: Unit = runProgram("test/R5RS/gambit/sboyer.scm", analysis)
+  @Benchmark
+  def scheme: Unit = runProgram("test/R5RS/gambit/scheme.scm", analysis)
+}
+
+@OutputTimeUnit(TimeUnit.MILLISECONDS)
+@BenchmarkMode(Array(Mode.SingleShotTime))
+class PerformanceEvaluationJMH1CFA extends PerformanceEvaluationJMH {
+  def analysis = SchemeAnalyses.kCFAAnalysis(_, 1)
+
+  @Benchmark
+  def mceval: Unit = runProgram("test/R5RS/various/mceval.scm", analysis)
+  @Benchmark
+  def leval: Unit = runProgram("test/R5RS/icp/icp_3_leval.scm", analysis)
+  @Benchmark
+  def sboyer: Unit = runProgram("test/R5RS/gambit/sboyer.scm", analysis)
+}
