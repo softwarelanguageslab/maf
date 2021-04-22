e55d2759d1d0b254c45c6c7335bfa00f002b6140
Jens Van der Plas <jevdplas@vub.be>
2021-04-22 12:56:27 +0200

    WIP Products

--- code/shared/src/main/scala/maf/lattice/instances/taint/AddressTaintLattice.scala
+++ code/shared/src/main/scala/maf/lattice/instances/taint/AddressTaintLattice.scala
@@ -24,9 +24,11 @@ object AddressTaintLattice {
   class AddressTaintLattice extends TaintLattice[TaintSet, T] {
     override def inject(t: TaintSet): T[TaintSet] = Taints(t)
     override def bottom: T[TaintSet] = Taints(Set())
-    override def join(x: T[TaintSet], y: => T[TaintSet]): T[TaintSet] = { case (Taints(x), Taints(y)) => Taints(x.union(y)) }
-    override def subsumes(x: T[TaintSet], y: => T[TaintSet]): Boolean = { case (Taints(x), Taints(y)) => y.subsetOf(x) }
-    override def eql[B: BoolLattice](x: T[TaintSet], y: T[TaintSet]): B = { case (Taints(x), Taints(y)) => BoolLattice[B].inject(x == y) }
+    override def join(x: T[TaintSet], y: => T[TaintSet]): T[TaintSet] = (x, y) match { case (Taints(x), Taints(y)) => Taints(x.union(y)) }
+    override def subsumes(x: T[TaintSet], y: => T[TaintSet]): Boolean = (x, y) match { case (Taints(x), Taints(y)) => y.subsetOf(x) }
+    override def eql[B: BoolLattice](x: T[TaintSet], y: T[TaintSet]): B = (x, y) match {
+      case (Taints(x), Taints(y)) => BoolLattice[B].inject(x == y)
+    }
     override def show(v: T[TaintSet]): String = v.toString
   }
 }
---
+++ code/shared/src/main/scala/maf/lattice/interfaces/product/ProductLattice.scala
@@ -0,0 +1,54 @@
+package maf.lattice.interfaces.product
+
+import maf.core.Lattice
+import maf.lattice.interfaces.BoolLattice
+
+// TODO maybe: make the product type opaque?
+class ProductLattice[L: Lattice, R: Lattice] extends Lattice[(L, R)] {
+
+  def bottom: (L, R) = (Lattice[L].bottom, Lattice[R].bottom)
+
+  def join(x: (L, R), y: => (L, R)): (L, R) = (Lattice[L].join(x._1, y._1), Lattice[R].join(x._2, y._2))
+
+  def subsumes(x: (L, R), y: => (L, R)): Boolean = Lattice[L].subsumes(x._1, y._1) && Lattice[R].subsumes(x._2, y._2)
+
+  def eql[B: BoolLattice](x: (L, R), y: (L, R)): B = BoolLattice[B].join(Lattice[L].eql(x._1, y._1), Lattice[R].eql(x._2, y._2))
+
+  def show(v: (L, R)): String = s"(${Lattice[L].show(v._1)} × ${Lattice[R].show(v._2)})"
+
+  /** First projection. */
+  def left(x: (L, R)): L = x._1
+
+  /** Second projection. */
+  def right(x: (L, R)): R = x._2
+
+  /** First projection of join. */
+  def joinLeft(x: (L, R), y: => (L, R)): L = Lattice[L].join(x._1, y._1)
+
+  /** Second projection of join. */
+  def joinRight(x: (L, R), y: => (L, R)): R = Lattice[R].join(x._2, y._2)
+
+  /** Returns an element of this lattice consisting of the operation applied to the first projection and the second projection. */
+  def opLeft1(
+      op: L => L,
+      x: (L, R)
+    ): (L, R) = (op(x._1), x._2)
+
+  /** Returns an element of this lattice consisting of the operation applied to the first projections and the join of the second projections. */
+  def opLeft2(
+      op: (L, L) => L,
+      x: (L, R),
+      y: (L, R)
+    ): (L, R) = (op(x._1, y._1), Lattice[R].join(x._2, y._2))
+
+  /** Returns an element of this lattice consisting of the join of the first projections and the operation applied to the second projections. */
+  def opRight2(
+      op: (R, R) => R,
+      x: (L, R),
+      y: (L, R)
+    ): (L, R) = (Lattice[L].join(x._1, y._1), op(x._2, y._2))
+}
+
+object ProductLattice {
+  def apply[L, R](implicit p: ProductLattice[L, R]): ProductLattice[L, R] = p
+}
