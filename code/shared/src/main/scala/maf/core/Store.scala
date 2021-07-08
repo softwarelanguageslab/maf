e0e675d7c23e6dcbe0256ed8fcd0db87944442ce
Noah Van Es <noahvanes@me.com>
2021-07-08 11:23:22 +0200

    add a Store with simple abstract GC

--- code/shared/src/main/scala/maf/core/Store.scala
+++ code/shared/src/main/scala/maf/core/Store.scala
@@ -1,6 +1,7 @@
 package maf.core
 
 import maf.util.SmartHash
+import scala.annotation.tailrec
 
 case class UnboundAddress[A <: Address](a: A) extends Error
 
@@ -34,18 +35,56 @@ trait Store[A <: Address, V] extends SmartHash { store =>
   def updateOrExtend(a: A, v: V): This = extend(a, v)
 }
 
-case class BasicStore[A <: Address, V: Lattice](content: Map[A, V]) extends Store[A, V] {
-  type This = BasicStore[A, V]
+trait MapStore[A <: Address, V] extends Store[A, V] { outer => 
+
+  type This >: this.type <: MapStore[A, V] { type This = outer.This }
+
+  implicit val lattice: Lattice[V]
+
+  val content: Map[A, V]
+  def updateContent(other: Map[A,V]): This
+
   def lookup(a: A): Option[V] = content.get(a)
   def extend(a: A, v: V): This = content.get(a) match {
-    // case None if v == Lattice[V].bottom => this  <- not necessary
-    case None => BasicStore(content + (a -> v))
+    case None if Lattice[V].isBottom(v) => this
+    case None => updateContent(content + (a -> v))
     case Some(oldValue) =>
       val newValue = Lattice[V].join(oldValue, v)
       if (oldValue == newValue) {
         this
       } else {
-        BasicStore(content + (a -> newValue))
+        updateContent(content + (a -> newValue))
       }
   }
+}
+
+trait AbstractGC[A <: Address, V] extends MapStore[A, V] {
+  // trim the content of a store
+  def trimContent(other: Map[A,V]): This  
+  // get all references of a value
+  def refs(v: V): Set[A]
+  // stop-and-copy style GC
+  def collect(roots: Set[A]): This =
+    scan(roots, roots, Map.empty)
+  @tailrec
+  private def scan(toMove: Set[A], moved: Set[A], current: Map[A,V]): This =
+    if(toMove.isEmpty) {
+      trimContent(current) 
+    } else {
+      val addr = toMove.head
+      val rest = toMove.tail
+      if (moved(addr)) {
+        scan(rest, moved, current)
+      } else {
+        val absv = this.content(addr)
+        val refs = this.refs(absv)
+        scan(rest ++ refs, moved + addr, current + (addr -> absv))
+      }
+    }
+}
+
+case class BasicStore[A <: Address, V: Lattice](content: Map[A,V]) extends MapStore[A, V] {
+  type This = BasicStore[A,V]
+  val lattice = Lattice[V]
+  def updateContent(other: Map[A,V]): BasicStore[A,V] = BasicStore(other)
 }
