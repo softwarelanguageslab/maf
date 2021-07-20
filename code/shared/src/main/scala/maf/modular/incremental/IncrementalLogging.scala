11353d83d32988e456de2614b917d9a1efa68a1d
Jens Van der Plas <jevdplas@vub.be>
2021-07-20 09:40:42 +0200

    Improvements for incremental logger

--- code/shared/src/main/scala/maf/modular/incremental/IncrementalLogging.scala
+++ code/shared/src/main/scala/maf/modular/incremental/IncrementalLogging.scala
@@ -20,8 +20,24 @@ trait IncrementalLogging[Expr <: Expression] extends IncrementalGlobalStore[Expr
   var step: Int = 0 // The current intra-component analysis.
   var botRead: Option[Addr] =
     None // The analysis of a component (sometimes) stops when reading bottom. Keep whether bottom was the last read value, and if so, the corresponding address read.
+  var repeats: Map[Component, Integer] = Map.empty.withDefaultValue(0) // Keep track of how many times every component has been analysed.
 
-  def focus(a: Addr): Boolean = a.toString == "PtrAddr((cons vars vals) [Some(ε)])" // Whether to "watch"" an address and insert it into the table.
+  def focus(a: Addr): Boolean = false // Whether to "watch"" an address and insert it into the table.
+
+  private def legend(): String =
+    """***** LEGEND OF ABBREVIATIONS *****
+      |ANLY  Analysis of a component, indicating the step of the analysis and the number of times the current component is now analysed.
+      |COMI  Indicates the component's analysis is committed.
+      |IUPD  Incremental update of the given address, indicating the value now residing in the store and the value actually written.
+      |PROV  Registration of provenance, including the address and new provenance value.
+      |READ  Address read, includes the address and value retrieved from the store.
+      |RSAD  Indicates the address dependencies for a given component are reset.
+      |WRIT  Address write, including the address, value written and value now residing in the store.
+      |
+      |***** ANALYSIS SUMMARY TABLE *****
+      |The column "bot" indicates whether bottom was read from the store (including the address), and hence whether the analysis of a component was prematurely terminated.
+      |""".stripMargin
+  logger.logU(legend())
 
   private def insertTable(messageOrComponent: Either[String, Component]): Unit = {
     val stepString = step.toString
@@ -119,7 +135,6 @@ trait IncrementalLogging[Expr <: Expression] extends IncrementalGlobalStore[Expr
     ): Boolean = {
     val b = super.updateAddrInc(cmp, addr, nw)
     logger.log(s"IUPD $addr <<= ${inter.store.getOrElse(addr, lattice.bottom)} (W $nw)")
-    //    logger.log(provenance(addr).toList.map({case (c, p) => s"          * $c :: $p"}).mkString("\n"))
     b
   }
 
@@ -130,7 +145,8 @@ trait IncrementalLogging[Expr <: Expression] extends IncrementalGlobalStore[Expr
     abstract override def analyzeWithTimeout(timeout: Timeout.T): Unit = {
       logger.logU("") // Adds a newline to the log.
       if (version == Old) intraC += 1 else intraCU += 1
-      logger.log(s"ANLY $component")
+      repeats = repeats + (component -> (repeats(component) + 1))
+      logger.log(s"ANLY $component (analysis step $step, analysis # of this component: ${repeats(component)})")
       if (configuration.cyclicValueInvalidation) logger.log(s"RSAD Resetting addressDependencies for $component.")
       super.analyzeWithTimeout(timeout)
     }
