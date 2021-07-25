84d7c58894836328ad5076297a7652339f913aa0
Noah Van Es <noahvanes@me.com>
2021-07-25 17:09:20 +0200

    only include value flows to VarAddr/PtrAddr

--- code/shared/src/main/scala/maf/modular/scheme/modflocal/SchemeModFLocalAnalysisResults.scala
+++ code/shared/src/main/scala/maf/modular/scheme/modflocal/SchemeModFLocalAnalysisResults.scala
@@ -1,6 +1,7 @@
 package maf.modular.scheme.modflocal
 
 import maf.modular._
+import maf.modular.scheme._
 import maf.language.scheme._
 import maf.core._
 
@@ -10,12 +11,18 @@ trait SchemeModFLocalAnalysisResults extends SchemeModFLocal with AnalysisResult
   var resultsPerIdn = Map.empty.withDefaultValue(Set.empty)
 
   override protected def extendV(sto: Store[Adr, Storable], adr: Adr, vlu: Val): sto.This = {
-    resultsPerIdn += adr.idn -> (resultsPerIdn(adr.idn) + vlu)
+    adr match {
+      case _: VarAddr[_] | _: PtrAddr[_] => 
+        resultsPerIdn += adr.idn -> (resultsPerIdn(adr.idn) + vlu)
+    }
     super.extendV(sto, adr, vlu)
   }
 
   override protected def updateV(sto: Store[Adr, Storable], adr: Adr, vlu: Val): sto.This = {
-    resultsPerIdn += adr.idn -> (resultsPerIdn(adr.idn) + vlu)
+    adr match {
+      case _: VarAddr[_] | _: PtrAddr[_] =>
+        resultsPerIdn += adr.idn -> (resultsPerIdn(adr.idn) + vlu)
+    }
     super.updateV(sto, adr, vlu)
   }
 
