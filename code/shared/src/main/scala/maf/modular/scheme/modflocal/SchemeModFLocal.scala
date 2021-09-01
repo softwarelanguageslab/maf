a80d470f52655bdf12e1595e07548f57dbb82559
Noah Van Es <noahvanes@me.com>
2021-09-01 15:10:22 +0200

    simplify type definition for `A`

--- code/shared/src/main/scala/maf/modular/scheme/modflocal/SchemeModFLocal.scala
+++ code/shared/src/main/scala/maf/modular/scheme/modflocal/SchemeModFLocal.scala
@@ -122,8 +122,8 @@ abstract class SchemeModFLocal(prg: SchemeExp) extends ModAnalysis[SchemeExp](pr
   //
 
   def withRestrictedStore[X](rs: Set[Adr])(blk: A[X]): A[X] =
-    A { (res, ctx, env, sto) =>
-      val (rss, cps, rds, wds) = blk.run(res, ctx, env, sto.collect(rs))
+    (res, ctx, env, sto) => {
+      val (rss, cps, rds, wds) = blk(res, ctx, env, sto.collect(rs))
       (rss.map { case (x, d) => (x, sto.replay(d)) }, cps, rds, wds)
     }
 
@@ -136,34 +136,33 @@ abstract class SchemeModFLocal(prg: SchemeExp) extends ModAnalysis[SchemeExp](pr
   // ANALYSISM MONAD
   //
 
-  // TODO: more type safety coming with path-dependent functions in Scala 3!
-  case class A[X](run: (res: Res, ctx: Ctx, env: Env, sto: Sto) => (Set[(X, sto.DeltaStore)], Set[Cmp], Set[Dep], Set[Dep]))
+  // TODO: make this an opaque type
+  type A[X] = (res: Res, ctx: Ctx, env: Env, sto: Sto) => (Set[(X, sto.DeltaStore)], Set[Cmp], Set[Dep], Set[Dep])
 
-  implicit override val analysisM: AnalysisM[A] = new AnalysisM[A] {
+  given analysisM: AnalysisM[A] with {
     // MONAD
     def unit[X](x: X) =
-      A((_, _, _, sto) => (Set((x, sto.deltaStore)), Set(), Set(), Set()))
+      (_, _, _, sto) => (Set((x, sto.deltaStore)), Set(), Set(), Set())
     def map[X, Y](m: A[X])(f: X => Y) =
-      A { (res, ctx, env, sto) =>
-        val (rss, cps, rds, wds) = m.run(res, ctx, env, sto)
+      (res, ctx, env, sto) => {
+        val (rss, cps, rds, wds) = m(res, ctx, env, sto)
         (rss.map(res => (f(res._1), res._2)), cps, rds, wds)
       }
     def flatMap[X, Y](m: A[X])(f: X => A[Y]) =
-      A { (res, ctx, env, sto) =>
-        val (rs1, cs1, rd1, wd1) = m.run(res, ctx, env, sto)
+      (res, ctx, env, sto) =>
+        val (rs1, cs1, rd1, wd1) = m(res, ctx, env, sto)
         rs1.foldLeft((Set[(Y, sto.DeltaStore)](), cs1, rd1, wd1)) { case (acc, (x, d0)) =>
-          val (rs2, cs2, rd2, wd2) = f(x).run(res, ctx, env, sto.integrate(d0))
+          val (rs2, cs2, rd2, wd2) = f(x)(res, ctx, env, sto.integrate(d0))
           val rs3 = rs2.map { case (x, d1) => (x, sto.compose(d1, d0)) }
           (acc._1 ++ rs3, acc._2 ++ cs2, acc._3 ++ rd2, acc._4 ++ wd2)
         }
-      }
     // MONADJOIN
     def mbottom[X] =
-      A((_, _, _, _) => (Set(), Set(), Set(), Set()))
+      (_, _, _, _) => (Set(), Set(), Set(), Set())
     def mjoin[X: Lattice](x: A[X], y: A[X]) =
-      A { (res, ctx, env, sto) =>
-        val (rs1, cs1, rd1, wd1) = x.run(res, ctx, env, sto)
-        val (rs2, cs2, rd2, wd2) = y.run(res, ctx, env, sto)
+      (res, ctx, env, sto) => {
+        val (rs1, cs1, rd1, wd1) = x(res, ctx, env, sto)
+        val (rs2, cs2, rd2, wd2) = y(res, ctx, env, sto)
         (rs1 ++ rs2, cs1 ++ cs2, rd1 ++ rd2, wd1 ++ wd2)
       }
     // MONADERROR
@@ -171,27 +170,27 @@ abstract class SchemeModFLocal(prg: SchemeExp) extends ModAnalysis[SchemeExp](pr
       mbottom // we are not interested in errors here (at least, not yet ...)
     // STOREM
     def addrEq =
-      A((_, _, _, sto) => (Set((sto.addrEq, sto.deltaStore)), Set(), Set(), Set()))
+      (_, _, _, sto) => (Set((sto.addrEq, sto.deltaStore)), Set(), Set(), Set())
     def extendSto(adr: Adr, vlu: Val) =
-      A((_, _, env, sto) => (Set(((), extendV(sto, adr, vlu))), Set(), Set(), Set()))
+      (_, _, env, sto) => (Set(((), extendV(sto, adr, vlu))), Set(), Set(), Set())
     def updateSto(adr: Adr, vlu: Val) =
-      A((_, _, env, sto) => (Set(((), updateV(sto, adr, vlu))), Set(), Set(), Set()))
+      (_, _, env, sto) => (Set(((), updateV(sto, adr, vlu))), Set(), Set(), Set())
     def lookupSto(adr: Adr) =
-      A((_, _, _, sto) => (Set((lookupV(sto, adr), sto.deltaStore)), Set(), Set(), Set()))
+      (_, _, _, sto) => (Set((lookupV(sto, adr), sto.deltaStore)), Set(), Set(), Set())
     // ANALYSISM
     def getCtx =
-      A((_, ctx, _, sto) => (Set((ctx, sto.deltaStore)), Set(), Set(), Set()))
+      (_, ctx, _, sto) => (Set((ctx, sto.deltaStore)), Set(), Set(), Set())
     // ENV STUFF
     def getEnv =
-      A((_, _, env, sto) => (Set((env, sto.deltaStore)), Set(), Set(), Set()))
+      (_, _, env, sto) => (Set((env, sto.deltaStore)), Set(), Set(), Set())
     def withExtendedEnv[X](nam: String, adr: Adr)(blk: A[X]): A[X] =
-      A((res, ctx, env, sto) => blk.run(res, ctx, env.extend(nam, adr), sto))
+      (res, ctx, env, sto) => blk(res, ctx, env.extend(nam, adr), sto)
     def extendEnvSto(adr: EnvAddr, evs: Set[Env]) =
-      A((_, _, _, sto) => (Set(((), extendE(sto, adr, evs))), Set(), Set(), Set()))
+      (_, _, _, sto) => (Set(((), extendE(sto, adr, evs))), Set(), Set(), Set())
     def lookupEnvSto(adr: EnvAddr) =
-      A((_, _, _, sto) => (Set(((lookupE(sto, adr), sto.deltaStore))), Set(), Set(), Set()))
+      (_, _, _, sto) => (Set(((lookupE(sto, adr), sto.deltaStore))), Set(), Set(), Set())
     def call(lam: Lam, ctx: Ctx): A[Val] =
-      A { (res, _, _, sto) =>
+      (res, _, _, sto) => {
         val cmp = CallComponent(lam, ctx, sto)
         val rss = res.getOrElse(cmp, Set.empty).asInstanceOf[Set[(Val, sto.DeltaStore)]]
         (rss, Set(cmp), Set(ResultDependency(cmp)), Set())
@@ -206,7 +205,7 @@ abstract class SchemeModFLocal(prg: SchemeExp) extends ModAnalysis[SchemeExp](pr
     var results = inter.results
 
     def analyzeWithTimeout(timeout: Timeout.T): Unit = {
-      val (res, cps, rds, wds) = eval(cmp.exp).run(results, cmp.ctx, cmp.env, cmp.sto)
+      val (res, cps, rds, wds) = eval(cmp.exp)(results, cmp.ctx, cmp.env, cmp.sto)
       val old = results.get(cmp)
       if (res != old) {
         results = results + (cmp -> res)
