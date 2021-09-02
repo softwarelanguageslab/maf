d8f7f081767faadba451e38c1899659b9ae04510
Noah Van Es <noahvanes@me.com>
2021-09-02 12:21:22 +0200

    elide context in SchemeAddr for context-insensitive analyses

--- code/shared/src/main/scala/maf/modular/scheme/SchemeAddresses.scala
+++ code/shared/src/main/scala/maf/modular/scheme/SchemeAddresses.scala
@@ -8,16 +8,19 @@ import maf.language.scheme._
  * @tparam Context
  *   A type of Contexts that can be used to distinguish more addresses. The type context is covariant.
  */
-trait SchemeAddr[+Context] extends Address
+trait SchemeAddr[+Context] extends Address {
+  protected def showCtx(ctx: Any) = 
+    if (ctx == ()) then "" else s"[$ctx]"
+}
 case class VarAddr[Context](id: Identifier, ctx: Context) extends SchemeAddr[Context] {
   def printable = true
   def idn: Identity = id.idn
-  override def toString: String = s"VarAddr(${id.fullString} [$ctx])"
+  override def toString: String = s"VarAddr(${id.fullString})${showCtx(ctx)}"
 }
 case class PtrAddr[Context](exp: SchemeExp, ctx: Context) extends SchemeAddr[Context] {
   def printable = false
   def idn: Identity = exp.idn
-  override def toString: String = s"PtrAddr($exp [$ctx])"
+  override def toString: String = s"PtrAddr($exp)${showCtx(ctx)}"
 }
 case class PrmAddr(nam: String) extends SchemeAddr[Nothing] {
   def printable = false
