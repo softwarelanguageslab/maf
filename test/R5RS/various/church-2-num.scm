15c5ad118013bd7b09fd66d32a407eb75a54b57f
Noah Van Es <noahvanes@me.com>
2021-09-02 09:48:04 +0200

    remove dead code in benchmark

--- test/R5RS/various/church-2-num.scm
+++ test/R5RS/various/church-2-num.scm
@@ -2,7 +2,4 @@
          (inc (lambda (n)
                 (lambda (f x)
                   (f (n f x)))))
-         (plus (lambda (m n)
-                 (lambda (f x)
-                   (m f (n f x))))))
   ((inc (inc zero)) (lambda (x) (+ x 1)) 0))
