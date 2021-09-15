63b36d4d580c053c3150c6536fc30d0baf4b7646
Bram Vandenbogaerde <contact@bramvdb.com>
2021-09-15 15:46:26 +0200

    wip jvm sat solver using z3

---
+++ code/jvm/src/main/scala/maf/cli/modular/scv/JVMSatSolver.scala
@@ -0,0 +1,70 @@
+package maf.cli.modular.scv
+
+import maf.modular.scv.{IsSat, ScvSatSolver}
+import maf.core.Address
+import maf.language.scheme._
+import maf.language.sexp.Value
+import maf.language.scheme.lattices.SchemeLattice
+
+class JVMSatSolver[V](using SchemeLattice[V, Address]) extends ScvSatSolver[V]:
+    /** A mapping between the name of Scheme primitives and their Z3 counter-parts */
+    private val primMap: Map[String, String] = Map(
+      "number?" -> "number?/v",
+      "boolean?" -> "boolean?/v",
+      "string?" -> "string?/v",
+      "true?" -> "true?/v",
+      "false?" -> "false?/v",
+      "null?" -> "null?/v",
+      "real?" -> "real?/v",
+      "integer?" -> "integer?/v",
+    )
+
+    /** Translates a symbolic Scheme value to an instance of the `V` sort */
+    private def injectValue(value: Value): String = value match
+        case Value.String(value)   => s"(VString $value)"
+        case Value.Symbol(symbol)  => throw new Exception("Not supported") // TODO
+        case Value.Integer(value)  => s"(VInteger $value)"
+        case Value.Real(value)     => s"(VReal $value)"
+        case Value.Boolean(b) if b => s"(VBool true)"
+        case Value.Boolean(b)      => s"(VBool false)"
+        case Value.Character(c)    => throw new Exception("Not supported") // TODO
+        case Value.Nil             => s"(VNil)"
+
+    /** A SMTLIB2 program that will be prepended to the actual constraints generated b our analyses */
+    private val prelude: String = """
+     | ;; represent the Scheme/Racket types as Z3 types 
+     | ;; TODO: check how cons, vector, ptr, and procedures can be represented, maybe we will need some representation of the heap?
+     |  (declare-datatypes ()
+     |    ((V (VInteger (unwrap-integer Int))
+     |        (VReal    (unwrap-real    Real))
+     |        (VNil)
+     |        (VBool    (unwrap-bool    Bool))
+     |        (VString  (unwrap-string  String))
+     |        (VError))))
+     |
+     |  (define-fun boolean?/v ((b V))
+     |     (VBool (is-VBool b)))
+     |
+     |  (define-fun null?/v ((n V))
+     |     (VBool (is-VNil n)))
+     |
+     |  (define-fun string?/v ((s V))
+     |     (VBool (is-VString n)))
+     |
+     |  (define-fun integer?/v ((n V)) V
+     |     (VBool (is-VInteger n)))
+     |
+     |  (define-fun number?/v ((n V)) V 
+     |    (VBool (or (is-VReal n) (is-VInteger n))))
+     |
+     |  (define-fun real?/v ((n V)) V
+     |     (VBool (is-VReal n)))
+     |
+     |  (define-fun true?/v ((n V)) Bool
+     |     (ite (is-VBool n) (unwrap-bool n) false))
+     |  (define-fun false?/v ((b V)) BOol
+     |     (ite (is-VBool n) (unwrap-bool) false))
+    """.stripMargin
+
+    /** Returns either Sat, Unsat or Unknown depending on the answer of Z3 */
+    def sat(e: SchemeExp): IsSat[V] = ???
