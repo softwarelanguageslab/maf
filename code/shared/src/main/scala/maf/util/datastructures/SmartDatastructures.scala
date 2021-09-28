b7f976c70a888f2b8460011aad799ed57f1acee8
Jens Van der Plas <jevdplas@vub.be>
2021-09-28 11:04:19 +0200

    Remove "SmartAppend"

--- code/shared/src/main/scala/maf/util/datastructures/SmartDatastructures.scala
+++ code/shared/src/main/scala/maf/util/datastructures/SmartDatastructures.scala
@@ -6,6 +6,3 @@ package maf.util.datastructures
 object SmartUnion:
     def sunion[A <: B, B](s1: Set[A], s2: Set[B]): Set[B] = if s1.size < s2.size then s2 ++ s1 else s1 ++ s2
     def sunionList[A](s: List[Set[A]]): Set[A] = s.foldLeft(Set.empty)(sunion)
-
-object SmartAppend:
-    def sappend[E](l1: List[E], l2: List[E]): List[E] = if l1.size < l2.size then l1 ::: l2 else l2 ::: l1
