bc092f002f11010eab42a0213b19d1ee5a38e439
Bram Vandenbogaerde <contact@bramvdb.com>
2021-09-28 12:10:50 +0200

    fixed test2: use real?/c instead of real/c

--- test/scv/manual/safe/test2.scm
+++ test/scv/manual/safe/test2.scm
@@ -1,7 +1,7 @@
 ;; tests path conditions, should also be safe because the monitor is only applied 
 ;; when the real? check has passed.
 
-(define real/c (flat real?))
+(define real?/c (flat real?))
 (define x (fresh)) ;; (fresh) generates an opaque value that receives a fresh symbolic representation
 (if (real? x)
     (mon real?/c x)
