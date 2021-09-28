a94227a3f33f9299944ab7d75e40715d8312d94e
Bram Vandenbogaerde <contact@bramvdb.com>
2021-09-28 14:57:10 +0200

    try: enable scv safety tests in CI

---
+++ code/jvm/src/test/scala/modular/scv/ScvSafetyTests.scala
@@ -0,0 +1,46 @@
+package maf.test.modular.scv
+
+import maf.cli.modular.scv._
+import maf.test._
+import maf.language.scheme._
+import maf.language.ContractScheme._
+import maf.language.scheme._
+import maf.language.scheme.lattices._
+import maf.modular._
+import maf.modular.scv._
+import maf.modular.scheme._
+import maf.modular.scheme.modf._
+import maf.modular.worklist._
+import maf.util._
+
+/**
+ * This type of test checks whether the analysis is able to obtain the expected precision.
+ *
+ * Precision in this case is measured whether it is able to elliminate program paths that introduce blames but that are actually infeasible at runtime
+ */
+trait ScvSafetyTests extends SchemeBenchmarkTests:
+    protected def analysis(program: SchemeExp) =
+      new ModAnalysis(program)
+        with ScvBigStepSemantics
+        with SchemeConstantPropagationDomain
+        with StandardScvModFComponents
+        with LIFOWorklistAlgorithm[SchemeExp]
+        with SchemeModFSemantics
+        with SchemeModFNoSensitivity:
+          override def intraAnalysis(cmp: Component) = new IntraScvSemantics(cmp)
+          override val sat: ScvSatSolver[Value] =
+              given SchemeLattice[Value, Addr] = lattice
+              new JVMSatSolver()
+
+    protected def parse(program: String): SchemeExp =
+      ContractSchemeParser.parse(program.nn)
+
+    override def onBenchmark(b: Benchmark): Unit =
+      property(s"$b should contain no contract violations") {
+        val content = Reader.loadFile(b)
+        val an = analysis(parse(content))
+        an.analyze()
+        assert(an.summary.blames.size == 0)
+      }
+
+class SimpleSafetyTests extends ScvSafetyTests with ContractSafetyTestsBenchmarks
