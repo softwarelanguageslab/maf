33dda93a63650fefbfa7e1fda53af1db059a8d63
Bram Vandenbogaerde <contact@bramvdb.com>
2021-09-29 15:48:44 +0200

    benchmarks: transform nguyen benchmarks to work in ContractScheme

--- test/scv/NguyenGTH18/safe/octy/ex-02.rkt
+++ test/scv/NguyenGTH18/safe/octy/ex-02.rkt
@@ -2,11 +2,19 @@
 ;; the issue here is that it has no knowledge about the or when analysing the function
 ;; so it cannot verify this correctly
 ;
-(define (f x)
+
+(define number?/c (flat number?))
+(define string?/c (flat string?))
+(define any?/c (flat (lambda (v) #t)))
+(define (or/c contract1 contract2) 
+  (flat (lambda (v) (or (check contract1 v) (check contract2 v)))))
+
+(define/contract (add1 x)
+   (-> number?/c number?/c)
+   (+ x 1))
+
+(define/contract (f x)
+  (-> (or/c string?/c number?/c) number?/c)
   (if (number? x) (add1 x) (string-length x)))
 
-(provide/contract 
-  (f (-> (or/c string? number?) number?)))
-
-(@unchecked f OPQ)
-(safe)
+(f (fresh))
