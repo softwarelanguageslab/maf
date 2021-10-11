ecdff49ef9e1c80b5b8f1d00cb4986512eabff5d
Noah Van Es <noahvanes@me.com>
2021-10-11 15:19:06 +0200

    don't use inner class definitions for delta stores

--- code/shared/src/main/scala/maf/modular/scheme/modflocal/SchemeModFLocal.scala
+++ code/shared/src/main/scala/maf/modular/scheme/modflocal/SchemeModFLocal.scala
@@ -17,7 +17,7 @@ abstract class SchemeModFLocal(prg: SchemeExp) extends ModAnalysis[SchemeExp](pr
     type Cmp = Component
     type Dep = Dependency
     type Sto = LocalStore[Adr, Val]
-    type Dlt = Sto#Delta
+    type Dlt = Delta[Adr, Val]
     type Cnt = AbstractCount
 
     //
@@ -54,7 +54,7 @@ abstract class SchemeModFLocal(prg: SchemeExp) extends ModAnalysis[SchemeExp](pr
         val exp = initialExp
         val env = initialEnv
         val ctx = initialCtx
-        val sto = emptySto
+        val sto = initialSto
         override def toString = "main"
     case class CallComponent(lam: Lam, env: Env, ctx: Ctx, sto: Sto) extends Component:
         def exp = SchemeBody(lam.body)
@@ -76,16 +76,14 @@ abstract class SchemeModFLocal(prg: SchemeExp) extends ModAnalysis[SchemeExp](pr
     // STORE STUFF
     //
 
-    lazy val emptySto: Sto = LocalStore.empty
-
-    def extendV(sto: Sto, stw: Sto, adr: Adr, vlu: Val): (sto.Delta, stw.Delta) = 
+    def extendV(sto: Sto, stw: Sto, adr: Adr, vlu: Val): (Dlt, Dlt) = 
       policy(adr) match
-        case AddrPolicy.Local => (sto.extend(adr, vlu), stw.emptyDelta)
-        case AddrPolicy.Widened => (sto.emptyDelta, stw.extend(adr, vlu))
-    def updateV(sto: Sto, stw: Sto, adr: Adr, vlu: Val): (sto.Delta, stw.Delta) = 
+        case AddrPolicy.Local => (sto.extend(adr, vlu), Delta.empty)
+        case AddrPolicy.Widened => (Delta.empty, stw.extend(adr, vlu))
+    def updateV(sto: Sto, stw: Sto, adr: Adr, vlu: Val): (Dlt, Dlt) = 
       policy(adr) match 
-        case AddrPolicy.Local => (sto.update(adr, vlu), stw.emptyDelta)
-        case AddrPolicy.Widened => (sto.emptyDelta, stw.update(adr, vlu))
+        case AddrPolicy.Local => (sto.update(adr, vlu), Delta.empty)
+        case AddrPolicy.Widened => (Delta.empty, stw.update(adr, vlu))
     def lookupV(sto: Sto, stw: Sto, adr: Adr): Val = 
       policy(adr) match 
         case AddrPolicy.Local => sto(adr)
@@ -118,19 +116,19 @@ abstract class SchemeModFLocal(prg: SchemeExp) extends ModAnalysis[SchemeExp](pr
             ((to._1, stw), rfs)
 
     object StoreGC extends AGC[Sto]:
-      def fresh1(cur: Sto) = emptySto
+      def fresh1(cur: Sto) = LocalStore.empty
       def move1(addr: Adr, from: Sto, to: Sto): (Sto, Set[Adr]) = 
         from.content.get(addr) match
             case None             => (to, Set.empty)
             case Some(s @ (v, _)) => (LocalStore(to.content + (addr -> s)), lattice.refs(v))
 
-    object DeltaGC extends AGC[Dlt]:
-      def fresh1(cur: Dlt) = cur.copy(delta = Map.empty) //TODO: this always carries over the set of updated addrs
-      def move1(addr: Adr, from: Dlt, to: Dlt): (Dlt, Set[Adr]) =
-        from.delta.get(addr).orElse(from.parent.content.get(addr)) match
-            case None                                       => (to, Set.empty)
-            case Some((v, _)) if !from.delta.contains(addr) => (to, lattice.refs(v))
-            case Some(s @ (v, _))                           => (to.copy(delta = to.delta + (addr -> s)), lattice.refs(v))
+    case object DeltaGC extends AGC[(Dlt,Sto)]:
+      def fresh1(cur: (Dlt, Sto)) = (cur._1.copy(delta = Map.empty), cur._2) //TODO: this always carries over the set of updated addrs
+      def move1(addr: Adr, from: (Dlt, Sto), to: (Dlt, Sto)): ((Dlt, Sto), Set[Adr]) =
+        from._1.delta.get(addr).orElse(from._2.content.get(addr)) match
+            case None                                           => (to, Set.empty)
+            case Some((v, _)) if !from._1.delta.contains(addr)  => (to, lattice.refs(v))
+            case Some(s @ (v, _))                               => ((to._1.copy(delta = to._1.delta + (addr -> s)), to._2), lattice.refs(v))
 
     def withRestrictedStore(rs: Set[Adr])(blk: A[Val]): A[Val] =
       (res, ctx, env, sto, stw) =>
@@ -139,10 +137,10 @@ abstract class SchemeModFLocal(prg: SchemeExp) extends ModAnalysis[SchemeExp](pr
           val (rs1, dw1) = collectDelta(gcs, gcw, rs0, dw0)
           (rs1.map((v,d) => (v, sto.replay(gcs, d))), stw.replay(gcw, dw1), ef0)
 
-    def collectDelta(sto: Sto, stw: Sto, rss: Set[(Val, sto.Delta)], std: stw.Delta): (Set[(Val, sto.Delta)], stw.Delta)  =
-      rss.foldLeft((Set.empty[(Val, sto.Delta)], stw.emptyDelta)) {
+    def collectDelta(sto: Sto, stw: Sto, rss: Set[(Val, Dlt)], std: Dlt): (Set[(Val, Dlt)], Dlt)  =
+      rss.foldLeft((Set.empty[(Val, Dlt)], Delta.empty)) {
         case ((accR, accW), (v, d)) =>
-          val (gcd: sto.Delta, gdw: stw.Delta) = DeltaGC.collect((d, std), lattice.refs(v) ++ d.updates)
+          val ((gcd, _), (gdw, _)) = DeltaGC.collect(((d, sto), (std, stw)), lattice.refs(v) ++ d.updates)
           (accR + ((v, gcd)), stw.join(accW, gdw))
       }
 
@@ -161,7 +159,7 @@ abstract class SchemeModFLocal(prg: SchemeExp) extends ModAnalysis[SchemeExp](pr
       case Widened 
 
     var fixedPolicies: Map[Adr, AddrPolicy] = Map.empty
-    def customPolicy(adr: Adr): AddrPolicy = AddrPolicy.Widened
+    def customPolicy(adr: Adr): AddrPolicy = AddrPolicy.Local
     def policy(adr: Adr): AddrPolicy = 
       fixedPolicies.get(adr) match
         case Some(ply) => ply
@@ -171,8 +169,8 @@ abstract class SchemeModFLocal(prg: SchemeExp) extends ModAnalysis[SchemeExp](pr
 
     override def init() = 
       super.init()
-      stores += MainComponent -> initialSto
-      initialBds.map(_._2).foreach(adr => fixedPolicies += adr -> AddrPolicy.Widened)
+      //stores += MainComponent -> initialSto
+      //initialBds.map(_._2).foreach(adr => fixedPolicies += adr -> AddrPolicy.Widened)
     
     case class WidenedAddrDependency(cmp: Cmp, adr: Adr) extends Dependency
 
@@ -192,12 +190,12 @@ abstract class SchemeModFLocal(prg: SchemeExp) extends ModAnalysis[SchemeExp](pr
     // ANALYSISM MONAD
     //
 
-    type A[X] = (res: Res, ctx: Ctx, env: Env, sto: Sto, stw: Sto) => (Set[(X, sto.Delta)], stw.Delta, Eff)
+    type A[X] = (Res, Ctx, Env, Sto, Sto) => (Set[(X, Dlt)], Dlt, Eff)
 
     given analysisM: AnalysisM[A] with
         // MONAD
         def unit[X](x: X) =
-          (_, _, _, sto, stw) => (Set((x, sto.emptyDelta)), stw.emptyDelta, Eff.empty)
+          (_, _, _, sto, stw) => (Set((x, Delta.empty)), Delta.empty, Eff.empty)
         def map[X, Y](m: A[X])(f: X => Y) =
           (res, ctx, env, sto, stw) =>
             val (rss, dlw, eff) = m(res, ctx, env, sto, stw)
@@ -206,7 +204,7 @@ abstract class SchemeModFLocal(prg: SchemeExp) extends ModAnalysis[SchemeExp](pr
           (res, ctx, env, sto0, stw0) =>
               val (rs1, dw1, ef1) = m(res, ctx, env, sto0, stw0)
               val stw1 = stw0.integrate(dw1)
-              rs1.foldLeft((Set.empty[(Y, sto0.Delta)], stw0.emptyDelta, ef1)) { case (acc, (x, d1)) =>
+              rs1.foldLeft((Set.empty[(Y, Dlt)], Delta.empty, ef1)) { case (acc, (x, d1)) =>
                 val sto1 = sto0.integrate(d1)
                 val (rs2, dw2, ef2) = f(x)(res, ctx, env, sto1, stw1)
                 val rs3 = rs2.map((x, d2) => (x, sto0.compose(d2, d1)))
@@ -214,7 +212,7 @@ abstract class SchemeModFLocal(prg: SchemeExp) extends ModAnalysis[SchemeExp](pr
               }
         // MONADJOIN
         def mbottom[X] =
-          (_, _, _, _, stw) => (Set.empty, stw.emptyDelta, Eff.empty)
+          (_, _, _, _, stw) => (Set.empty, Delta.empty, Eff.empty)
         def mjoin[X: Lattice](x: A[X], y: A[X]) =
           (res, ctx, env, sto, stw) =>
             val (rs1, dw1, ef1) = x(res, ctx, env, sto, stw)
@@ -225,7 +223,7 @@ abstract class SchemeModFLocal(prg: SchemeExp) extends ModAnalysis[SchemeExp](pr
           mbottom // we are not interested in errors here (at least, not yet ...)
         // STOREM
         def addrEq =
-          (_, _, _, sto, stw) => (Set((eqA(sto), sto.emptyDelta)), stw.emptyDelta, Eff.empty)
+          (_, _, _, sto, stw) => (Set((eqA(sto), Delta.empty)), Delta.empty, Eff.empty)
         def extendSto(adr: Adr, vlu: Val) =
           (_, _, env, sto, stw) => 
             val (std, sdw) = extendV(sto, stw, adr, vlu)
@@ -236,15 +234,15 @@ abstract class SchemeModFLocal(prg: SchemeExp) extends ModAnalysis[SchemeExp](pr
             (Set(((), std)), sdw, Eff.empty)
         def lookupSto(adr: Adr) =
           (_, _, _, sto, stw) => 
-            (Set((lookupV(sto, stw, adr), sto.emptyDelta)), stw.emptyDelta, Eff.empty)
+            (Set((lookupV(sto, stw, adr), Delta.empty)), Delta.empty, Eff.empty)
         // CTX STUFF
         def getCtx =
-          (_, ctx, _, sto, stw) => (Set((ctx, sto.emptyDelta)), stw.emptyDelta, Eff.empty)
+          (_, ctx, _, sto, stw) => (Set((ctx, Delta.empty)), Delta.empty, Eff.empty)
         def withCtx[X](f: Ctx => Ctx)(blk: A[X]): A[X] =
           (res, ctx, env, sto, stw) => blk(res, f(ctx), env, sto, stw)
         // ENV STUFF
         def getEnv =
-          (_, _, env, sto, stw) => (Set((env, sto.emptyDelta)), stw.emptyDelta, Eff.empty)
+          (_, _, env, sto, stw) => (Set((env, Delta.empty)), Delta.empty, Eff.empty)
         def withEnv[X](f: Env => Env)(blk: A[X]): A[X] =
           (res, ctx, env, sto, stw) => blk(res, ctx, f(env), sto, stw)
         // CALL STUFF
@@ -252,7 +250,7 @@ abstract class SchemeModFLocal(prg: SchemeExp) extends ModAnalysis[SchemeExp](pr
           (res, ctx, env, sto, stw) =>
             val cmp = CallComponent(lam, env, ctx, sto)
             val eff = Eff.call(cmp) ++ Eff.write(stw.content.map({ case (adr, (vlu, cnt)) => (cmp, adr, cnt, vlu) }))
-            val (rss, dlw) = res.getOrElse(cmp, (Set.empty, stw.emptyDelta)).asInstanceOf[(Set[(Val, sto.Delta)], stw.Delta)]
+            val (rss, dlw) = res.getOrElse(cmp, (Set.empty, Delta.empty)).asInstanceOf[(Set[(Val, Dlt)], Dlt)]
             (rss, dlw, eff)
 
     //
@@ -266,7 +264,7 @@ abstract class SchemeModFLocal(prg: SchemeExp) extends ModAnalysis[SchemeExp](pr
       var stores = inter.stores
 
       def analyzeWithTimeout(timeout: Timeout.T): Unit =
-          val stw = stores.getOrElse(cmp, emptySto)
+          val stw = stores.getOrElse(cmp, LocalStore.empty)
           val (res, stw1, eff) = eval(cmp.exp)(results, cmp.ctx, cmp.env, cmp.sto, stw)
           val rgc = collectDelta(cmp.sto, stw, res, stw1)
           // update result of the analysed component
@@ -283,7 +281,7 @@ abstract class SchemeModFLocal(prg: SchemeExp) extends ModAnalysis[SchemeExp](pr
           }
           // process writes to widened stores
           eff.w.foreach { case (cmp, adr, cnt, vlu) =>
-            val prv = stores.getOrElse(cmp, emptySto)
+            val prv = stores.getOrElse(cmp, LocalStore.empty)
             prv.joinAt(adr, vlu, cnt) match
               case None => ()
               case Some(sto) => 
@@ -299,7 +297,7 @@ abstract class SchemeModFLocal(prg: SchemeExp) extends ModAnalysis[SchemeExp](pr
                 true
             else false
           case WidenedAddrDependency(cmp, adr) =>
-            val oldS = inter.stores.getOrElse(cmp, emptySto)
+            val oldS = inter.stores.getOrElse(cmp, LocalStore.empty)
             val newS = intra.stores(cmp) // we are certain to have a store here!
             val (newV, newC) = newS.content(adr)
             oldS.joinAt(adr, newV, newC) match 
