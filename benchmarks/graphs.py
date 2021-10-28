3c7aad2765381f380941fe728487c5e3ef981f7a
Quentin Stiévenart <quentin.stievenart@gmail.com>
2021-10-28 12:28:01 +0200

    [actions] Generate performance graph, try 1

--- benchmarks/graphs.py
+++ benchmarks/graphs.py
@@ -13,20 +13,18 @@ def read_metrics(filename):
         metrics = json.loads(contents)
         for metric in metrics:
             output.append(dict(name = metric["benchmark"], time = metric["primaryMetric"]["score"]))
-        
     return output
 
 
-alldirs = glob.glob("extracted/*")
-dates = [ d.replace("extracted/", "").split("_")[0] for d in alldirs  ]
+alldirs = glob.glob("artifact/*") ++ glob.glob("code/jvm/jmh-results-*.json")
+dates = [ d.replace("artifact/", "").split("_")[0] for d in alldirs  ]
 metrics = [ read_metrics(glob.glob(d+"/*.json")[0]) for d in alldirs ]
 for date, group in zip(dates, metrics):
     for metric in group:
         metric["date"] = date
         metric["name"] = metric["name"].replace("maf.cli.experiments.performance.PerformanceEvaluation", "")
- 
-df = pd.DataFrame(sum(metrics, []))
 
+df = pd.DataFrame(sum(metrics, []))
 df["date"] = pd.to_datetime(df.date)
 
 import seaborn as sb
