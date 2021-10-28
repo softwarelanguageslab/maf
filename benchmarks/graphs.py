63d08c35b12df24c173772f5b96bbac2c29b48c3
Quentin Stiévenart <quentin.stievenart@gmail.com>
2021-10-28 16:20:40 +0200

    [actions] Try to get current performance results in graph.py

--- benchmarks/graphs.py
+++ benchmarks/graphs.py
@@ -4,7 +4,7 @@
 import pandas as pd
 import json
 import glob
-
+import os.path
 
 def read_metrics(filename):
     output = []
@@ -15,8 +15,15 @@ def read_metrics(filename):
             output.append(dict(name = metric["benchmark"], time = metric["primaryMetric"]["score"]))
     return output
 
+current_results = glob.glob("code/jvm/jmh-results-*.json")
+if current_results == []:
+    print('No current performance results')
+    exit(1)
 
-alldirs = glob.glob("artifact/*") + glob.glob("code/jvm/jmh-results-*.json")
+print(current_results)
+os.system("mv '%s' artifact/")
+
+alldirs = glob.glob("artifact/*")
 dates = [ d.replace("artifact/", "").split("_")[0] for d in alldirs  ]
 metrics = [ read_metrics(glob.glob(d+"/*.json")[0]) for d in alldirs ]
 for date, group in zip(dates, metrics):
