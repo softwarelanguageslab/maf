88b2b922e4c70d81a49afef01dd306197ca91099
Bram Vandenbogaerde <contact@bramvdb.com>
2021-12-01 16:00:00 +0100

    [scv] hack contracts into lexical addressing

--- code/shared/src/main/scala/maf/language/scheme/SchemeLexicalAddresser.scala
+++ code/shared/src/main/scala/maf/language/scheme/SchemeLexicalAddresser.scala
@@ -79,6 +79,14 @@ trait BaseSchemeLexicalAddresser:
           val bdsLex = vrs.zip(translate(eps, extEnv))
           val bdyLex = translateBody(body, extEnv)
           SchemeLetrec(bdsLex, bdyLex, pos)
+        // TODO: HACK: ContractScheme expression should no be here, but no easy way for MutableVarBoxer
+        // to make this parametric
+        case ContractSchemeMon(contract, expression, idn) =>
+          ContractSchemeMon(translate(contract, lenv), translate(expression, lenv), idn)
+        case ContractSchemeFlatContract(contract, idn) =>
+          ContractSchemeFlatContract(translate(contract, lenv), idn)
+        case ContractSchemeDepContract(domains, rangeMaker, idn) =>
+          ContractSchemeDepContract(domains.map(translate(_, lenv)), translate(rangeMaker, lenv), idn)
         case _ => throw new Exception(s"Unsupported Scheme expression: $exp")
 
     def translate(bdy: List[SchemeExp], lenv: LexicalEnv): List[SchemeExp] =
