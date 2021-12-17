b447a6f18f797064b2c6e2c1fb0758238d89cc94
Bram Vandenbogaerde <bram@vdb.space>
2021-12-17 11:53:08 +0100

    [aam] sensitivity: 1-m-cfa

---
+++ code/shared/src/main/scala/maf/aam/scheme/sensitivity/SchemeAAMContextInsensitivity.scala
@@ -0,0 +1,14 @@
+package maf.aam.scheme.sensitivity
+
+import maf.aam.scheme.*
+
+import maf.core.*
+import maf.language.scheme.SchemeFuncall
+
+trait SchemeAAMContextInsensitivity extends BaseSchemeAAMSemantics:
+    type Timestamp = Unit
+    val initialTime: Timestamp = ()
+    def tick(timestamp: Timestamp, e: Expr, sto: Sto, kont: KonA): Timestamp = ()
+    def alloc(identity: Identity, env: Env, sto: Sto, kont: KonA, ctx: Timestamp): Address =
+      VarAddr(identity, ctx)
+    def allocCtx(fexp: SchemeFuncall, t: Timestamp): Timestamp = t
