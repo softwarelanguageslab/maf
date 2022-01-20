a43c027a9fdb80cc0ac4af01ff219840e1b2f43a
Jens Van der Plas <jevdplas@vub.be>
2022-01-20 15:05:24 +0100

    Disallow double definitions in letrec

--- code/shared/src/main/scala/maf/language/scheme/SchemeExp.scala
+++ code/shared/src/main/scala/maf/language/scheme/SchemeExp.scala
@@ -205,6 +205,7 @@ case class SchemeLetrec(
         .toSet
     val label: Label = LTR
     def letName: String = "letrec"
+    if bindings.size > bindings.map(_._1.name).toSet.size then throw new Exception(s"Illegal letrec: duplicate definitions (${idn.pos}).")
 
 /** Named-let: (let name ((v1 e1) ...) body...) */
 object SchemeNamedLet:
