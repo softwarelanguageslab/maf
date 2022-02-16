acf90a6b208b026d8b521182c73a243495f92929
Bram Vandenbogaerde <contact@bramvdb.com>
2022-02-16 11:56:44 +0100

    fix random generator in test code

--- code/shared/src/test/scala/maf/test/language/ContractScheme/RandomGenerator.scala
+++ code/shared/src/test/scala/maf/test/language/ContractScheme/RandomGenerator.scala
@@ -6,5 +6,5 @@ import maf.language.ContractScheme.interpreter.*
 /** A generator that generates random input regardless of the contract and topLevelFunction parameter */
 class PurelyRandomGenerator(primitives: List[String]) extends RandomInputGenerator:
     private val generator: ConcreteGenerators = ConcreteGenerators(primitives)
-    override def generateInput(topLevelFunction: String, contract: Set[String] = Set()): List[ConcreteValues.Value] =
-      List(generator.any.sample.get)
+    override def generateInput(topLevelFunction: String, contract: Set[String] = Set()): List[InputGenerator] =
+      List(noalloc(generator.any.sample.get))
