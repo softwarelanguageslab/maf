21751e0b4ab4bf7860ab3375c4ad2d1431da0d76
Bram Vandenbogaerde <contact@bramvdb.com>
2022-06-06 12:32:07 +0200

    enable summary (without topsort) performance benchmark again

--- code/jvm/src/main/scala/maf/cli/experiments/aam/Performance.scala
+++ code/jvm/src/main/scala/maf/cli/experiments/aam/Performance.scala
@@ -69,7 +69,7 @@ object ScvPerformanceComparison extends AAMPerformanceComparison:
         List(
           //(wrap(AAMAnalyses.scvAAMFnCallBoundaries), "scvAAMFfn"),
           (wrapModF(SchemeAnalyses.scvModAnalysisWithRacketFeatures), "scvModf"),
-          //(wrapModF(SchemeAnalyses.scvModAnalysisFunctionSummary), "scvFunctionSummaries"),
+          (wrapModF(SchemeAnalyses.scvModAnalysisFunctionSummary), "scvFunctionSummaries"),
           (wrapModF(SchemeAnalyses.scvModAnalysisFunctionSummaryTopSort), "scvFunctionSummariesTopSort"),
           (wrapModF(SchemeAnalyses.scvModAnalysisRktFsR), "scvRktFsR")
           //(wrapModF(SchemeAnalyses.scvModAnalysisWithRacketFeaturesWithPathSensitiveStore), "scvModF-sensitive"),
