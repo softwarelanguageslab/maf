5ee677d123c3d3ec778344805c59d0c865c84a21
Bram Vandenbogaerde <contact@bramvdb.com>
2022-06-20 12:33:11 +0200

    updated results visualisation scripts for scv

--- scripts/Python/scv/performance/compare.py
+++ scripts/Python/scv/performance/compare.py
@@ -10,6 +10,13 @@ import matplotlib.pyplot as plt
 from matplotlib.lines import Line2D
 import sys
 
+# A statically defined set of groups
+GROUPS = [
+        #"softy",
+        #"sergey", 
+        #"mochi"
+]
+
 def load_dataframe(filename):
     """
     Load the data from the CSV file, and parse it into the correct format
@@ -28,30 +35,129 @@ def load_dataframe(filename):
     df["collection phase (ms)"] = df["collection phase (ms)"] / (1000*1000)
     df["propagation phase (ms)"] = df["propagation phase (ms)"] / (1000*1000)
     df["z3 (ns)"] = df["z3 (ns)"] / (1000*1000)
-    print(df)
     return df 
 
+
+def name_of_group(composite):
+    name, configuration = composite
+    for groupName in GROUPS:
+        if name.startswith(groupName): 
+            return (groupName, configuration)
+    return (name, configuration)
 
 def summarize_table(df):
     df1 = df.assign(name = df["name"].map(lambda x: '/'.join(x.split("/")[-2:])))
     df1 = df1.assign(blames = df1["time"].astype(float))
-    df1 = df1[["name", "time", "configuration"]]
+    df1 = df1[["name", "time", "configuration", "propagation phase (ms)", "collection phase (ms)"]]
     df2 = df1.set_index(["name", "configuration"])
-    return df2.unstack()
+    collapsed = df2.groupby(name_of_group)
+    result = pd.DataFrame().assign(
+        median_time = collapsed["time"].median(),
+        max_time = collapsed["time"].max(),
+        min_time = collapsed["time"].min(),
+        avg_time = collapsed["time"].mean(),
+        std_dev = collapsed["time"].std(),
+        avg_prop = collapsed["propagation phase (ms)"].mean(),
+        avg_coll = collapsed["collection phase (ms)"].mean()
+    )
+
+    result.index = pd.MultiIndex.from_tuples(result.index)
+    unstacked = result.unstack()
+    unstacked["speedup"] = unstacked["avg_time"]["(2)"] / unstacked["avg_time"]["(4)"] 
+    
+    return unstacked
+
+def load_std(filename):
+    """
+    Load the standard deviation table into a DataFrame. Setting up the index on the name of the benchmark.
+
+    :param filename the name of the file to load 
+    """
+
+    df = pd.read_csv(filename)
+    df["name"] = df["name"].map(lambda x: "/".join(x.split("/")[-2:]))
+    df = df.set_index("name")
+    df.columns = df.columns.str.split("_", expand = True).rename("configuration", level = 0)
+    unstacked = df.stack(0).unstack()
+    selected = unstacked[["time"]].replace(["TIMEOUT", "_"], np.nan)
+    print(selected)
+
+
+def to_latex(df, time_column = "median_time", time_column_readable = "Median Time (ms)"): 
+    """
+    Convert the given DataFrame to a Latex format that can be used in the paper.
+
+    :param df the dataframe as a result of summarize_table
+    """
+
+    selection = df[[time_column, "speedup"]]
+
+    # Convert speedup to percentage
+    selection = selection.assign(speedup = (selection["speedup"] - 1) * 100)
+    # Add percentage sign to it, and if bigger than one put in bold
+    bolded = selection["speedup"].apply(lambda x: "\\textbf{{{:10.2f}}}\%".format(x)) 
+    formatted = selection["speedup"].apply(lambda x: "{:10.2f}\%".format(x))
+    # Apply bolded on places where speedup > 0
+    highlighted = formatted.where(selection["speedup"] < 0, bolded)
+
+    # Underline the score that is the smallest in the median time   
+    ## Compute the smallest value of each row (but drop the first configuration)
+    minimum = selection[time_column].drop("(1)", axis=1).min(axis=1)
+    ## Broadcast the smallest value such that the column is repeated for each configuration
+    copied = selection.copy()
+    copied[time_column].loc[:, :] = minimum.values[:, None]
+    ## Compute the cell that has the minimum value
+    mask = selection[time_column] != copied[time_column]
+    ## Use the mask for conditional formatting
+    highlight_median = selection[time_column].applymap(lambda x: "\\underline{{{}}}".format(x))
+    selection[time_column] = selection[time_column].where(mask, highlight_median, axis = 1)
+
+    # Compute averages as an extra row
+    # avg =  selection.mean().rename("Average")
+    # print(avg)
+    # selection = selection.append(avg)
+
+    # Rename for printing
+    selection = selection.assign(speedup = highlighted)
+    selection = selection.rename(columns = {time_column: time_column_readable, "speedup": "Speedup"})
+    # Replace nan with -
+    selection = selection.replace({np.nan: "-", "nan\%": "-"})
+
+    return selection.to_latex(escape = False)
+
+def compute_propagation_phase_vs_collection_phase_ratio(df):
+    print("Collection Phase Slowdown Compared to the baseline")
+    selection = df[["avg_prop", "avg_coll", "median_time"]]
+    slowdown = (selection["avg_coll"]["(3)"] / selection["median_time"]["(1)"])
+    print(slowdown)
+
+    print("Propagation vs Collection Phase Ratio")
+    ratio_coll = selection["avg_coll"]["(4)"] / selection["median_time"]["(4)"]
+    ratio_prop = selection["avg_prop"]["(4)"] / selection["median_time"]["(4)"]
+    print(pd.DataFrame().assign(ratio_coll = ratio_coll, ratio_prop = ratio_prop))
+    print(ratio_coll.mean(), ratio_prop.mean())
+
+
 
 FILENAME = sys.argv[1]
 df = load_dataframe(FILENAME)
 
 # Text rotation
 plt.xticks(rotation = 45, ha = "right")
-df = df.assign(configuration = df["configuration"].replace({"scvFunctionSummariesTopSort": "Proposed Approach", "scvModf": "Baseline", "scvRktFsR": "Nguyen et al."}))
-ax = sb.barplot(data = df, x = "name", y = "time", hue = "configuration", log = True)
-ax.set_xlabel("Benchmark")
-ax.set_ylabel("Time (ms)")
+df = df.assign(configuration = df["configuration"].replace({"scvFunctionSummariesTopSort": "(4)", "scvModf": "(1)", "scvRktFsR": "(2)", "scvFunctionSummaries": "(3)"}))
+# ax = sb.barplot(data = df, x = "name", y = "time", hue = "configuration", log = True)
+# ax.set_xlabel("Benchmark")
+# ax.set_ylabel("Time (ms)")
 # Make sure that the rotated xticks fit in the figure
-
 df.to_csv("output.csv")
-summarize_table(df).to_csv(FILENAME+"-summary.csv")
+summary = summarize_table(df) # .to_csv(FILENAME+"-summary.csv")
 
-plt.tight_layout()
-plt.show()
+compute_propagation_phase_vs_collection_phase_ratio(summary)
+exit(0)
+
+load_std(FILENAME+"-stddev")
+print(to_latex(summary, "avg_time", "Average Time (ms)"))
+summary.to_csv(FILENAME+"-summary.csv")
+
+# plt.tight_layout()
+# plt.show()
--- scripts/Python/scv/reuse.py
+++ scripts/Python/scv/reuse.py
@@ -8,21 +8,34 @@ FILE = sys.argv[1]
 BENCH_FILE = sys.argv[2]
 
 df = pd.read_csv(FILE)
-df_bench = pd.read_csv(BENCH_FILE)[["name", "scvFunctionSummariesTopSort_time", "scvRktFsR_time", "scvFunctionSummariesTopSort_# components", "scvRktFsR_# components"]]
+df_bench = pd.read_csv(BENCH_FILE)[["name", "scvFunctionSummariesTopSort_time", "scvRktFsR_time", "scvFunctionSummariesTopSort_# components", "scvRktFsR_# components", "scvFunctionSummaries_z3 interpreter (ns)", "scvRktFsR_z3 interpreter (ns)", "scvFunctionSummariesTopSort_# cache hits" ]]
 df_bench_named = df_bench.set_index("name")
-df_bench_named["speedup"] =  df_bench_named["scvRktFsR_time"].replace(["_", "TIMEOUT"], np.Inf).astype(float) / df_bench_named["scvFunctionSummariesTopSort_time"].replace(["_", "TIMEOUT"], 1).astype(float)
+df_bench_named["speedup"] =  ((df_bench_named["scvRktFsR_time"].replace(["_", "TIMEOUT"], np.Inf).astype(float) / df_bench_named["scvFunctionSummariesTopSort_time"].replace(["_", "TIMEOUT"], 1).astype(float)) - 1) * 100
 print(df.groupby("name").sum()["metric"] / df_bench.set_index("name")["scvFunctionSummariesTopSort_# components"])
 df_bench_named["reuse"] = df.groupby("name").sum()["metric"].astype(float) / df_bench_named["scvFunctionSummariesTopSort_# components"].astype(float)
-df_bench_named["component_reduction"] = 1 - df_bench_named["scvFunctionSummariesTopSort_# components"] / df_bench_named["scvRktFsR_# components"].replace(["_", "TIMEOUT"], 1.0).astype(float)
-df_bench_named["got_speedup"] = df_bench_named["speedup"] > 1
+df_bench_named["component_reduction"] = (1 - df_bench_named["scvFunctionSummariesTopSort_# components"] / df_bench_named["scvRktFsR_# components"].replace(["_", "TIMEOUT"], 1.0).astype(float))*100
+df_bench_named["got_speedup"] = df_bench_named["speedup"] > 0
+df_bench_named["z3_fraction_summaries"] = (df_bench_named["scvFunctionSummaries_z3 interpreter (ns)"].replace("_", 0).astype(float) / (1000*1000)) / df_bench_named["scvFunctionSummariesTopSort_time"].replace(["_", "TIMEOUT"], np.Inf).astype(float)
+
+df_bench_named["z3_fraction_rktFsR"] = (df_bench_named["scvRktFsR_z3 interpreter (ns)"].replace("_", 0).astype(float) / (1000*1000)) / df_bench_named["scvRktFsR_time"].replace(["_", "TIMEOUT"], np.Inf).astype(float)
+df_bench_named["z3_cmp"] = df_bench_named["z3_fraction_summaries"] / df_bench_named["z3_fraction_rktFsR"]
+
 df_bench_with_speedup = df_bench_named.where(df_bench_named["component_reduction"] > -8).dropna()
 print(df_bench_with_speedup)
 
 # 1 row, 2 columns
-figure, axes = plt.subplots(2, 2, sharex=False, sharey=False)
-figure.suptitle("Performance")
+#@figure, axes = plt.subplots(2, 2, sharex=False, sharey=False)
+#@figure.suptitle("Performance")
+#@
+#@sb.scatterplot(ax = axes[0][0], data = df_bench_with_speedup, x = "reuse", y = "speedup", hue = "got_speedup")
+#@sb.scatterplot(ax = axes[0][1], data = df_bench_with_speedup, x = "component_reduction", y = "speedup", hue = "got_speedup")
+#@# sb.displot(ax = axes[1][0], data = df_bench_with_speedup, x = "component_reduction", hue = "got_speedup")
+#@sb.scatterplot(ax = axes[1][1], data = df_bench_with_speedup,  x = "z3_cmp", y = "speedup", hue = "got_speedup")
 
-sb.scatterplot(ax = axes[0][0], data = df_bench_with_speedup, x = "reuse", y = "speedup", hue = "got_speedup")
-sb.scatterplot(ax = axes[0][1], data = df_bench_with_speedup, x = "component_reduction", y = "speedup", hue = "got_speedup")
-sb.displot(ax = axes[1][0], data = df_bench_with_speedup, x = "component_reduction", hue = "got_speedup")
-plt.show()
+ax = sb.scatterplot(data = df_bench_with_speedup, x = "component_reduction", y = "speedup", hue = "got_speedup")
+ax.set_xlabel("Component Reduction (%)")
+ax.set_ylabel("Speedup")
+plt.legend([],[], frameon=False)
+
+plt.savefig("reduction.pdf")
+
