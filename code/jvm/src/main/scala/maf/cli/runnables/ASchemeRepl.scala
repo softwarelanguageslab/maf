6b7e8ea97f21bc9fa5a9dd60611d61f08ffe1d97
Bram Vandenbogaerde <contact@bramvdb.com>
2022-06-21 13:46:20 +0200

    extended actor scheme repl for testing

--- code/jvm/src/main/scala/maf/cli/runnables/ASchemeRepl.scala
+++ code/jvm/src/main/scala/maf/cli/runnables/ASchemeRepl.scala
@@ -2,9 +2,20 @@ package maf.cli.runnables
 
 import scala.io.StdIn
 import maf.language.AScheme.ASchemeParser
+import maf.language.scheme.SchemeBegin
+import maf.core.Identity
+import maf.modular.scheme.modactor.SimpleSchemeModActorAnalysis
+import maf.util.Reader
 
 object ASchemeRepl:
     def main(args: Array[String]): Unit =
+        print("> ")
         val input = StdIn.readLine().trim().nn
-        val parsed = ASchemeParser.parse(input)
-        println(parsed)
+        if input != ":q" then
+            val program = if !input.startsWith(":f") then input else Reader.loadFile(input.split(' ')(1).nn)
+            val parsed = ASchemeParser.parse(program)
+            val analysis = new SimpleSchemeModActorAnalysis(SchemeBegin(parsed, Identity.none))
+            analysis.analyze()
+            println(analysis.storeString())
+            main(args)
+        else ()
