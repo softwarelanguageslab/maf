0cbee3ffc5c1d52a67106040375e010bc0fe8065
Bram Vandenbogaerde <contact@bramvdb.com>
2022-08-02 11:15:00 +0200

    racket actor contracts: implemented syntax for behavior/c and primitive checking on the arguments of behaviors and messages

--- experiments/contracts/actor-contracts.rkt
+++ experiments/contracts/actor-contracts.rkt
@@ -18,8 +18,10 @@
   (let ((initial-behavior (apply behavior arguments)))
     (actor (thread (lambda ()
               (let loop ((behavior initial-behavior))
-                (let ((msg (thread-receive)))
-                  (loop (apply behavior (car msg) (cdr msg))))))))))
+                (let* ((msg (thread-receive))
+                       (tag (message-tag msg))
+                       (arguments (message-arguments msg)))
+                  (loop (apply behavior tag arguments)))))))))
 
 (define (become new-behavior . args)
   (apply new-behavior args))
@@ -34,7 +36,7 @@
 (define-syntax send/a
   (syntax-rules ()
     ((send/a receiver tag arg ...)
-     (actor-send receiver (cons (quote tag) (list arg ...))))))
+     (actor-send receiver (message (quote tag) (list arg ...))))))
 
 ;; Internal functions
 (define (actor-send actor datum) 
@@ -145,30 +147,60 @@
 
 (struct actor-mon (actor contract))
 
+;; a contract on the behavior of an actor 
+;; 
+;; @param constructor-contract a list of contracts on the constructor parameters of the behavior
+;; @param message-list a hashtable of mapping message tags to their contract
+(struct behavior/contract 
+   (constructor-contract message-list))
+
 ;; a contract on the handling of a message.
 ;; properties of interest are: existance of message handler .
 ;; ensuring that particular message sends happen.
 ;; ensuring that the actor ends up in a particular behavior.
-(struct message/c (expected-tag ensure-sends ensure-behaviors))
+(struct message/c (parameter-contracts expected-tag ensure-sends ensure-behaviors))
 
+;; Syntax for contract creation over the behavior of an actor
 (define-syntax behavior/c 
-  (syntax-rules ()
-     ((behavior/c (contructor-contract ...) 
-                 ((tag (param-contract ...)) ...))
-     'todo)))
+  (syntax-rules (ensure-send)
+     ((behavior/c (constructor-contract ...) 
+                 ((tag (param-contract ...) (ensure-send send ...) (ensure-becomes become ...)) ...))
+      (behavior/contract (list constructor-contract ...) 
+         (make-hash
+            (list (cons (quote tag) (message/c (list param-contract ...) (quote tag) (list send ...) (list become ...))) ...))))
+     ((behavior/c (constructor-contract ...)
+                  ((tag (param-contract ...) (ensure-send send ...)) ...))
+                  (behavior/c (constructor-contract ...) 
+                     ((tag (param-contract ...) (ensure-send send ...) (ensure-becomes)) ...)))
+     ((behavior/c (constructor-contract ...) 
+                  ((tag (param-contract ...)) ...))
+      (behavior/c (constructor-contract ...)
+                  ((tag (param-contract ...) (ensure-send) (ensure-becomes)) ...)))))
 
-(define (create/c contract behavior . args) 'todo)
+;; create a contracted actor reference
+(define (create/c beh/c behavior . args)
+   (let ((actor (apply create behavior args)))
+     ;; check whether the constructors hold for the arguments 
+     (for-each (lambda (argument c) 
+      (contract c argument 'pos 'neg))
+      args
+      (behavior/contract-constructor-contract beh/c))
+     (actor-mon actor beh/c)))
 
-;; A send on a contract entity, is used internally by `send` when it can dispatch to a contracted send.
+;; A send on a contracted entity, is used internally by `send` when it can dispatch to a contracted send.
 (define-syntax send/c 
   (syntax-rules () 
     ((send/c target tag arguments ...) 
-     (let 
-       ((target-contract (actor-mon-contract target)))
+     (let*
+       ((target-contract (actor-mon-contract target))
+        (message-contract (hash-ref (behavior/contract-message-list target-contract) (quote tag))))
        ;; check the arguments of the contract
-       'todo
-       ;; actually send the message but with the message/ contract attached 
-       (actor-send target (contracted-message (quote tag) (list arguments ...) (message/c (quote tag) 'todo 'todo)))))))
+       (for-each (lambda (c argument)
+                   (contract c argument 'pos 'neg))
+                 (message/c-parameter-contracts message-contract)
+                 (list arguments ...))
+       ;; actually send the message but with the message/c contract attached 
+       (actor-send (actor-mon-actor target) (contracted-message (quote tag) (list arguments ...) message-contract))))))
 
 (define-syntax send 
   (syntax-rules ()
@@ -186,6 +218,10 @@
   (behavior "player" ((team-id any?))
     (messages ((get-team (reply-to) (reply reply-to team-id))))))
 
-(define myplayer (create player 1))
+(define player/c (behavior/c 
+                   (integer?)
+                   ((get-team (actor?)))))
+
+(define myplayer (create/c player/c player 1))
 (define answer (await (ask myplayer get-team)))
 (displayln answer)
