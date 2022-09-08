27110b0fecea3a43740679280f09c3a1c2941ce1
bram <contact@bramvdb.com>
2022-09-08 16:12:42 +0200

    added more examples of mirror-usage

---
+++ test/concurrentScheme/actors/mirrors/counted.scm
@@ -0,0 +1,21 @@
+;; Example of actor that is only allowed to N messages. 
+
+(define N 5)
+(define mirror (mirror "mirror" (n)
+                       (send (interpreter m) 
+                             (if (= n 0)
+                               (begin (reply-to interpreter 'fail)
+                                      (error "could not send message"))
+                               (begin (reply-to interpreter 'ok)
+                                      (base/send-envelope m)
+                                      (become mirror (- n 1)))))
+                       (receive (interpreter msg handler) (reply-to interpreter handler))))
+
+(define my-behavior (actor "test" () 
+                           (do-send (n) 
+                                    ; should be blocked after five times
+                                    (send self do-send (+ n 1)))))
+
+(define my-actor (create-with-mirror (create mirror N) my-behavior))
+(send my-actor do-send 1)
+
