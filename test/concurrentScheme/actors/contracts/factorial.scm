da8b618ca34cb60cedb353f7f0234229b6534365
Bram Vandenbogaerde <contact@bramvdb.com>
2022-09-09 16:09:53 +0200

    wip contracts for savina and soter benchmark suite

---
+++ test/concurrentScheme/actors/contracts/factorial.scm
@@ -0,0 +1,30 @@
+(require contracts/actor)
+(require actor)
+
+;; From Agha, 1986, p. 54
+;; Adapted with contracts by Bram Vandenbogaerde
+(letrec ((fact-actor
+          (actor "fac" ()
+                 (compute (n customer)
+                          (if (= n 0)
+                              (send customer result 1)
+                              (let ((c (create customer-actor n customer)))
+                                (send self compute (- n 1) c)))
+                          (become fact-actor))))
+         (customer/c (lambda () (behavior/c (integer? (customer/c))
+                                            (ensure-sends-only/c (result (integer?))))))
+         (fact-actor/c (behavior/c () 
+                           (compute (integer? (customer/c))
+                                    (ensure-sends-one/c (result (integer?))
+                                                        (compute (integer? customer/c))))))
+         (customer-actor
+          (actor "customer" (n customer)
+                 (result (k)
+                         (send customer result (* n k))
+                         (become customer-actor n customer))))
+         (display-actor
+          (actor "display" ()
+                 (result (n) (display n))))
+         (f (create fact-actor))
+         (disp (create display-actor)))
+  (send f compute 5 disp))
