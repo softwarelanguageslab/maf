e4fc866328d8a0dffc6a4ccb484b8aa73347d5ac
Bram Vandenbogaerde <contact@bramvdb.com>
2022-09-28 13:54:05 +0200

    added abstract domain for actor contract values

---
+++ code/shared/src/main/scala/maf/language/AContractScheme/ASchemeLattice.scala
@@ -0,0 +1,20 @@
+package maf.language.AContractScheme
+
+import maf.core.*
+import maf.language.scheme.*
+import maf.language.scheme.lattices.*
+import maf.language.AContractScheme.AContractSchemeValues.*
+
+/** A lattice that includes contract values for the actor language */
+trait AContractSchemeLattice[L, A <: Address] extends SchemeLattice[L, A]:
+    /** Inject an ensures/c value in the abstract domain */
+    def ensuresC(nww: EnsuresC[L]): L
+
+    /** Inject a message/c value in the abstract domain */
+    def messageC(nww: MessageC[L]): L
+
+    /** Extract ensures/c values from the abstract domain */
+    def getMessageC(x: L): Set[MessageC[L]]
+
+    /** Extract ensures/c values from the abstract domain */
+    def getEnsuresC(y: L): Set[EnsuresC[L]]
