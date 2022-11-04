678bbc462a28b8cb8602e4d8ec8f56872c19af1b
Turgut12 <turgut.reis.kursun@vub.be>
2022-11-04 20:18:15 +0100

    prettify test

--- code/shared/src/test/scala/maf/test/TurgutsThesis/gtr/ShallowDeleteIdentifierTest.scala
+++ code/shared/src/test/scala/maf/test/TurgutsThesis/gtr/ShallowDeleteIdentifierTest.scala
@@ -18,88 +18,84 @@ class ShallowDeleteIdentifierTest extends AnyFlatSpec {
 
     //code 1: identifier in appl
     val programText1: String =
-      "(let " +
-        "((x 5))" +
-          "(+ 10 x)" +
-          "(x 5 9)" +
-          "(x x x))"
+    """(let
+      |    ((x 5))
+      |  (+ 10 x)
+      |  (x 5 9)
+      |  (x x x))""".stripMargin
 
     testCode(programText1)
 
     //code 2: identifier in if
     val programText2: String =
-      "(let " +
-        "((x 5))" +
-        "(define y 10)" +
-          "(if (= x 0) y y)" +
-          "(if (= y 0) (+ x 10) y)" +
-          "(if (= y 0) y (+ x 10)))"
+      """(let
+        |    ((x 5))
+        |  (define y 10)
+        |  (if (= x 0) y y)
+        |  (if (= y 0) (+ x 10) y)
+        |  (if (= y 0) y (+ x 10)))""".stripMargin
 
     testCode(programText2, "(let () (define y 10))")
 
     //code 3: identifier in set!
     val programText3: String =
-      "(let " +
-        "((x 5)" +
-         "(y 9))" +
-          "(set! y (+ x 10))" +
-          "(set! x (+ y y)))"
+      """(let
+        |    ((x 5)
+        |     (y 9))
+        |  (set! y (+ x 10))
+        |  (set! x (+ y y)))""".stripMargin
 
     testCode(programText3, "(let ((y 9)) )")
 
     //code 4: identifier in define
     val programText4: String =
-      "(let " +
-        "((x 5))" +
-          "(define y (+ 10 (+ x 10))))"
+      """(let
+        |    ((x 5))
+        |  (define y (+ 10 (+ x 10))))""".stripMargin
 
     testCode(programText4)
 
     //code 5: identifier in let/letStar/letrec
     val programText5: String =
-    "(let " +
-      "((x 5))" +
-        "(let ((y x)) 10)" +
-        "(let* ((z 10)) x)" +
-      ")"
+    """(let
+      |    ((x 5))
+      |  (let ((y x)) 10)
+      |  (let* ((z 10)) x))""".stripMargin
 
     testCode(programText5)
 
     //code 6: identifier in lambda
     val programText6: String =
-      "(let " +
-        "((x 5))" +
-          "(lambda (y z) (+ 100 (+ 100 x)))" +
-          ")"
+      """(let
+        |    ((x 5))
+        |  (lambda (y z) (+ 100 (+ 100 x)))
+        |  )""".stripMargin
 
     testCode(programText6)
 
     //code 7: identifier should not be deleted everywhere
 
-
     val programText7: String =
-      "(let " +
-        "((x 5)" +
-         "(a 10)" +
-         "(b 100))" +
-            "(lambda (y z) (+ 100 (+ 100 x)))" +
-            "(+ a b (* a 10))" +
-            "(if a a a)" +
-            "(if a (+ b b) (* b b))" +
-            "(set! a b)" +
-          ")"
+      """(let ((x 5)
+        |      (a 10)
+        |      (b 100))
+        |  (lambda (y z) (+ 100 (+ 100 x)))
+        |  (+ a b (* a 10))
+        |  (if a a a)
+        |  (if a (+ b b) (* b b))
+        |  (set! a b))""".stripMargin
 
 
     testCode(programText7, "(let ((a 10) (b 100)) (+ a b (* a 10)) (if a a a) (if a (+ b b) (* b b)) (set! a b))")
 
     //code 8: Let with more than 1 binding
     val programText8: String =
-      "(let " +
-        "((x 5)" +
-        " (y (+ 10 x)))" +
-        "(* y x)" +
-        "(begin 10 (+ y 10))" +
-        ")"
+      """(let
+        |    ((x 5)
+        |     (y (+ 10 x)))
+        |  (* y x)
+        |  (begin 10 (+ y 10)))
+        |""".stripMargin
 
     testCode(programText8)
 
@@ -113,6 +109,5 @@ class ShallowDeleteIdentifierTest extends AnyFlatSpec {
         |  )""".stripMargin
 
     testCode(programText9)
-
   }
 }
