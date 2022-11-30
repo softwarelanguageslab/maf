8c7b69275a9292af3bc2866eef70ece371fd3a76
Turgut12 <turgut.reis.kursun@vub.be>
2022-11-30 11:37:26 +0100

    also replace with constant lambdas & nil

--- code/shared/src/main/scala/maf/TurgutsThesis/gtr/transformations/traits/Replacing.scala
+++ code/shared/src/main/scala/maf/TurgutsThesis/gtr/transformations/traits/Replacing.scala
@@ -1,17 +1,31 @@
 package maf.TurgutsThesis.gtr.transformations.traits
 
 import maf.core.{Identifier, NoCodeIdentity}
-import maf.language.scheme.{SchemeExp, SchemeFuncall, SchemeValue, SchemeVarExp}
+import maf.language.scheme.{SchemeExp, SchemeFuncall, SchemeValue, SchemeVarExp, SchemeVar, SchemeVarArgLambda}
 import maf.language.sexp.Value
 
 trait Replacing:
   def replaceWithAllValues(exp: SchemeExp, toReplace: SchemeExp => Boolean): List[SchemeExp] =
     List(
-      replaceWithValue(exp, toReplace, Value.Integer(1)),
-      replaceWithValue(exp, toReplace, Value.String("S")),
-      replaceWithValue(exp, toReplace, Value.Boolean(true)),
-      replaceWithValue(exp, toReplace, Value.Boolean(false)),
-      replaceWithValue(exp, toReplace, Value.Symbol("S")),
+      replaceWithValue(exp, toReplace, SchemeValue(Value.Integer(1), NoCodeIdentity)),
+      replaceWithValue(exp, toReplace, SchemeValue(Value.String("S"), NoCodeIdentity)),
+      replaceWithValue(exp, toReplace, SchemeValue(Value.Symbol("S"), NoCodeIdentity)),
+      replaceWithValue(exp, toReplace, SchemeValue(Value.Boolean(true), NoCodeIdentity)),
+      replaceWithValue(exp, toReplace, SchemeValue(Value.Boolean(false), NoCodeIdentity)),
+      replaceWithValue(exp, toReplace, SchemeValue(Value.Nil, NoCodeIdentity)),
+      //lambdas:
+      replaceWithValue(exp, toReplace,
+        SchemeVarArgLambda(None, List(), Identifier("args", NoCodeIdentity), List(SchemeValue(Value.Integer(1), NoCodeIdentity)), None, NoCodeIdentity)),
+      replaceWithValue(exp, toReplace,
+        SchemeVarArgLambda(None, List(), Identifier("args", NoCodeIdentity), List(SchemeValue(Value.String("S"), NoCodeIdentity)), None, NoCodeIdentity)),
+      replaceWithValue(exp, toReplace,
+        SchemeVarArgLambda(None, List(), Identifier("args", NoCodeIdentity), List(SchemeValue(Value.Symbol("S"), NoCodeIdentity)), None, NoCodeIdentity)),
+      replaceWithValue(exp, toReplace,
+        SchemeVarArgLambda(None, List(), Identifier("args", NoCodeIdentity), List(SchemeValue(Value.Boolean(true), NoCodeIdentity)), None, NoCodeIdentity)),
+      replaceWithValue(exp, toReplace,
+        SchemeVarArgLambda(None, List(), Identifier("args", NoCodeIdentity), List(SchemeValue(Value.Boolean(false), NoCodeIdentity)), None, NoCodeIdentity)),
+      replaceWithValue(exp, toReplace,
+        SchemeVarArgLambda(None, List(), Identifier("args", NoCodeIdentity), List(SchemeValue(Value.Nil, NoCodeIdentity)), None, NoCodeIdentity)),
     )
 
   def replaceIdWithAllValues(exp: SchemeExp, id: Identifier): List[SchemeExp] =
@@ -22,9 +36,9 @@ trait Replacing:
         case _ => false
     })  
     
-  private def replaceWithValue(exp: SchemeExp, toReplace: SchemeExp => Boolean, value: Value): SchemeExp =
+  private def replaceWithValue(exp: SchemeExp, toReplace: SchemeExp => Boolean, value: SchemeExp): SchemeExp =
     exp.map(subExp => {
       if toReplace(subExp) then
-        SchemeValue(value, NoCodeIdentity)
+        value
       else subExp
     })
