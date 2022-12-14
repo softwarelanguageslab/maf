13fa72c321325af00f0c848758d45ffe62eddee2
Noah Van Es <noahvanes@me.com>
2022-12-14 16:46:42 +0100

    add context-sensitivity parameterisations

--- code/shared/src/main/scala/maf/aam/AAMScheme.scala
+++ code/shared/src/main/scala/maf/aam/AAMScheme.scala
@@ -10,10 +10,27 @@ import maf.language.sexp
 import maf.core.MonadJoin.MonadJoinIterableSyntax
 import maf.lattice.interfaces.BoolLattice
 
+trait AAMSchemeSensitivity extends AAMScheme:
+    this: SchemeDomain =>
+    // context is parameterisable 
+    type Context
+    def t0: Context
+    def tnext(app: App, clo: Clo, ctx: Ctx): Context
 
+trait AAMNoSensitivity extends AAMSchemeSensitivity:
+    this: SchemeDomain =>
+    type Context = Unit
+    def t0 = ()
+    def tnext(app: App, clo: Clo, ctx: Ctx): Context = ()
+
+trait AAMCallSiteSensitivity(k: Int) extends AAMSchemeSensitivity:
+    this: SchemeDomain =>
+    type Context = List[App]
+    def t0 = Nil
+    def tnext(app: App, clo: Clo, ctx: Ctx): Context = (app :: ctx).take(k)
 
 abstract class AAMScheme(prg: SchemeExp):
-    this: SchemeDomain => 
+    this: SchemeDomain with AAMSchemeSensitivity => 
 
     // shorthands
 
@@ -28,12 +45,6 @@ abstract class AAMScheme(prg: SchemeExp):
     type Env = Environment[Adr]
     type Sto = CountingStore[Adr, Val]
     type KSto = BasicStore[KAdr, Set[Frame]]
-
-    // parameters
-
-    type Context
-    def t0: Context
-    def tnext(app: App, clo: Clo, ctx: Ctx): Context
 
     // addresses
 
