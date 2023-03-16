68b72dd8783802bf7c7357006e4d6a1cf9e0302e
Bram Vandenbogaerde <bram@vdb.space>
2023-03-16 23:05:49 +0100

    docs

--- src/main/scala/maf/values/scheme/SchemeLattice.scala
+++ src/main/scala/maf/values/scheme/SchemeLattice.scala
@@ -3,6 +3,18 @@ package scheme
 
 import typeclasses.*
 
+/** Represents a lattice that supports all Scheme operations, that is: a
+  * combination of the operations on strings, integers, booleans, real numbers,
+  * symbols and characters.
+  *
+  * An implementation of this trait should represents the R5RS semantics for
+  * these operations. For example, R5RS says that if a real number and an
+  * integer number are added together, the result is a real number.
+  *
+  * The recommended representation of values in this lattice is an `HMap` which
+  * bundles several lattices together into a spare product lattice which has
+  * both efficient time and space characteristcs.
+  */
 trait SchemeLattice[L]
     extends IntLattice[L],
       StringLattice[L, L, L, L],
