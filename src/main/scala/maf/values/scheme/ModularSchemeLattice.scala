4772b9d9738e41a92d55df666723e5ad4e8cfffd
Bram Vandenbogaerde <bram@vdb.space>
2023-03-21 22:27:54 +0100

    formatting

--- src/main/scala/maf/values/scheme/ModularSchemeLattice.scala
+++ src/main/scala/maf/values/scheme/ModularSchemeLattice.scala
@@ -26,11 +26,234 @@ class ModularSchemeDomain[
   val SymT = LatKey.T[String, Sym]("symbol")
   val NilT = LatKey.T[Unit, Unit]("null")
 
-  given lattice: SchemeLattice[Val]
-    with SparseProductLattice[LatKey]
-    with {
-      //
-    }
+  given lattice: SchemeLattice[Val] with SparseProductLattice[LatKey] with {
+    type MonadError[M[_]] = cats.extensions.MonadError[Error][M]
+
+    given galois: Galois[ConcreteSchemeValue, Val] = ???
+
+    //
+    // toString
+    //
+
+    override def toString[C: CharLattice_[Val, Sym, S], S: StringLattice_[
+      Val,
+      C,
+      Sym
+    ]: GaloisFrom[String], Sym: SymbolLattice](n: Val): S = ???
+
+    override def toString[I: IntLattice, C: CharLattice_[
+      I,
+      Sym,
+      S
+    ], S: StringLattice_[
+      I,
+      C,
+      Sym
+    ]: GaloisFrom[String], Sym: SymbolLattice](n: Val): S = ???
+
+    def toString[I: IntLattice, C: CharLattice_[I, Val, S], S: StringLattice_[
+      I,
+      C,
+      Val
+    ]: GaloisFrom[String]](n: Val): S = ???
+
+    override def toString(c: Val): Val = ???
+
+    override def cons(car: Val, cdr: Val): Val = ???
+    override def downCase[M[_]: MonadError: MonadJoin](
+        c: Val
+    ): M[Val] = ???
+
+    override def toChar[C: CharLattice_[Val, Sym, S]: GaloisFrom[
+      Char
+    ], S: StringLattice_[
+      Val,
+      C,
+      Sym
+    ], Sym: SymbolLattice](n: Val): C = ???
+
+    def toReal[M[_]: MonadError: MonadJoin, R: RealLattice: GaloisFrom[
+      Double
+    ]](n: Val): M[R] = ???
+
+    override def upCase[M[_]: MonadError: MonadJoin](
+        c: Val
+    ): M[Val] = ???
+
+    override def toInt[M[_]: MonadError: MonadJoin, I: IntLattice: GaloisFrom[
+      BigInt
+    ]](
+        c: Val
+    ): M[I] = ???
+
+    override def isLower[M[
+        _
+    ]: MonadError: MonadJoin, B: BoolLattice: GaloisFrom[Boolean]](
+        c: Val
+    ): M[B] = ???
+
+    override def isUpper[M[
+        _
+    ]: MonadError: MonadJoin, B: BoolLattice: GaloisFrom[Boolean]](
+        c: Val
+    ): M[B] = ???
+
+    override def charEq[M[
+        _
+    ]: MonadError: MonadJoin, B: BoolLattice: GaloisFrom[Boolean]](
+        c1: Val,
+        c2: Val
+    ): M[B] = ???
+
+    override def charLt[M[
+        _
+    ]: MonadError: MonadJoin, B: BoolLattice: GaloisFrom[Boolean]](
+        c1: Val,
+        c2: Val
+    ): M[B] = ???
+
+    override def charEqCI[M[
+        _
+    ]: MonadError: MonadJoin, B: BoolLattice: GaloisFrom[Boolean]](
+        c1: Val,
+        c2: Val
+    ): M[B] = ???
+
+    override def charLtCI[M[
+        _
+    ]: MonadError: MonadJoin, B: BoolLattice: GaloisFrom[Boolean]](
+        c1: Val,
+        c2: Val
+    ): M[B] = ???
+
+    /** Inject a concrete value into the abstract domain, this function
+      * corresponds to the `alpha` function in a Galois connection
+      */
+    override def inject(c: ConcreteSchemeValue): Val = ???
+
+    /** Extract a set concrete values from the abstract domain, this function
+      * corresponds to the `gamma` function in a Galois connection
+      */
+    override def extract(a: Val): Option[Set[ConcreteSchemeValue]] =
+      ???
+
+    override def length[M[_]: MonadError: MonadJoin](
+        s: Val
+    ): M[Val] = ???
+
+    override def append[M[_]: MonadError: MonadJoin](
+        s1: Val,
+        s2: Val
+    ): M[Val] = ???
+
+    override def substring[M[_]: MonadError: MonadJoin](
+        s: Val,
+        from: Val,
+        to: Val
+    ): M[Val] = ???
+
+    override def ref[M[_]: MonadError: MonadJoin](
+        s: Val,
+        i: Val
+    ): M[Val] = ???
+
+    override def set[M[_]: MonadError: MonadJoin](
+        s: Val,
+        i: Val,
+        c: Val
+    ): M[Val] = ???
+
+    override def toSymbol[M[_]: MonadError: MonadJoin](
+        s: Val
+    ): M[Val] = ???
+
+    override def toNumber[M[_]: MonadError: MonadJoin](
+        s: Val
+    ): M[Val] = ???
+
+    override def makeString[M[_]: MonadError: MonadJoin](
+        length: Val,
+        char: Val
+    ): M[Val] = ???
+
+    override def quotient[M[_]: MonadError: MonadJoin](
+        n1: Val,
+        n2: Val
+    ): M[Val] = ???
+
+    override def div[M[_], R: GaloisFrom[Double]](
+        n1: Val,
+        n2: Val
+    )(using
+        e1: cats.MonadError[M, Error],
+        e2: maf.values.typeclasses.MonadJoin[M],
+        e3: maf.values.typeclasses.RealLattice[R]
+    ): M[R] = ???
+
+    override def modulo[M[_]: MonadError: MonadJoin](
+        n1: Val,
+        n2: Val
+    ): M[Val] = ???
+
+    override def remainder[M[_]: MonadError: MonadJoin](
+        n1: Val,
+        n2: Val
+    ): M[Val] = ???
+
+    override def valuesBetween(n1: Val, n2: Val): Set[Val] = ???
+
+    override def ceiling[M[_]: MonadError: MonadJoin](n: Val): M[Val] = ???
+
+    override def floor[M[_]: MonadError: MonadJoin](n: Val): M[Val] = ???
+
+    override def round[M[_]: MonadError: MonadJoin](n: Val): M[Val] = ???
+
+    override def log[M[_]: MonadError: MonadJoin](n: Val): M[Val] = ???
+
+    override def random[M[_]: MonadError: MonadJoin](n: Val): M[Val] = ???
+
+    override def sin[M[_]: MonadError: MonadJoin](n: Val): M[Val] = ???
+
+    override def asin[M[_]: MonadError: MonadJoin](n: Val): M[Val] = ???
+
+    override def cos[M[_]: MonadError: MonadJoin](n: Val): M[Val] = ???
+
+    override def acos[M[_]: MonadError: MonadJoin](n: Val): M[Val] = ???
+
+    override def tan[M[_]: MonadError: MonadJoin](n: Val): M[Val] = ???
+
+    override def atan[M[_]: MonadError: MonadJoin](n: Val): M[Val] = ???
+
+    override def sqrt[M[_]: MonadError: MonadJoin](n: Val): M[Val] = ???
+
+    override def plus[M[_]: MonadError: MonadJoin](n1: Val, n2: Val): M[Val] =
+      ???
+
+    override def minus[M[_]: MonadError: MonadJoin](n1: Val, n2: Val): M[Val] =
+      ???
+
+    override def times[M[_]: MonadError: MonadJoin](n1: Val, n2: Val): M[Val] =
+      ???
+
+    override def div[M[_]: MonadError: MonadJoin](n1: Val, n2: Val): M[Val] =
+      ???
+
+    override def expt[M[_]: MonadError: MonadJoin](n1: Val, n2: Val): M[Val] =
+      ???
+
+    override def lt[M[_]: MonadError: MonadJoin, B: BoolLattice: GaloisFrom[
+      Boolean
+    ]](
+        n1: Val,
+        n2: Val
+    ): M[B] = ???
+
+    override def isTrue(b: Val): Boolean = ???
+
+    override def isFalse(b: Val): Boolean = ???
+
+    override def not(b: Val): Val = ???
+  }
 
   //
   // Injection
--- src/main/scala/maf/values/typeclasses/RealLattice.scala
+++ src/main/scala/maf/values/typeclasses/RealLattice.scala
@@ -6,17 +6,19 @@ import maf.util.Error
 
 /** A lattice for reals (i.e., floating point numbers) */
 trait RealLattice[R] extends Lattice[R] { self =>
-  def toInt[M[_]: MonadError[Error]: MonadJoin, I: IntLattice: GaloisFrom[
-    BigInt
-  ]](n: R): M[I]
+  def toInt[
+      M[_]: MonadError[Error]: MonadJoin,
+      I: IntLattice: GaloisFrom[BigInt]
+  ](n: R): M[I]
   def ceiling[M[_]: MonadError[Error]: MonadJoin](n: R): M[R]
   def floor[M[_]: MonadError[Error]: MonadJoin](n: R): M[R]
   def round[M[_]: MonadError[Error]: MonadJoin](n: R): M[R]
-  def isZero[M[_]: MonadError[Error]: MonadJoin, B: BoolLattice: GaloisFrom[
-    Boolean
-  ]](v: R)(using
-      Galois[Double, R]
-  ): B =
+  def isZero[
+      M[_]: MonadError[Error]: MonadJoin,
+      B: BoolLattice: GaloisFrom[
+        Boolean
+      ]
+  ](v: R)(using Galois[Double, R]): B =
     eql(v, Galois.inject[Double, R](0))
   def log[M[_]: MonadError[Error]: MonadJoin](n: R): M[R]
   def random[M[_]: MonadError[Error]: MonadJoin](n: R): M[R]
@@ -32,14 +34,16 @@ trait RealLattice[R] extends Lattice[R] { self =>
   def times[M[_]: MonadError[Error]: MonadJoin](n1: R, n2: R): M[R]
   def div[M[_]: MonadError[Error]: MonadJoin](n1: R, n2: R): M[R]
   def expt[M[_]: MonadError[Error]: MonadJoin](n1: R, n2: R): M[R]
-  def lt[M[_]: MonadError[Error]: MonadJoin, B: BoolLattice: GaloisFrom[
-    Boolean
-  ]](n1: R, n2: R): M[B]
-  def toString[I: IntLattice, C: CharLattice_[I, Sym, S], S: StringLattice_[
-    I,
-    C,
-    Sym
-  ]: GaloisFrom[String], Sym: SymbolLattice](n: R): S
+  def lt[
+      M[_]: MonadError[Error]: MonadJoin,
+      B: BoolLattice: GaloisFrom[Boolean]
+  ](n1: R, n2: R): M[B]
+  def toString[
+      I: IntLattice,
+      C: CharLattice_[I, Sym, S],
+      S: StringLattice_[I, C, Sym]: GaloisFrom[String],
+      Sym: SymbolLattice
+  ](n: R): S
 }
 
 object RealLattice:
