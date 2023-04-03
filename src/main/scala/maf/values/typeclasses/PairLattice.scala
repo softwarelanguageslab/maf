3d6157a7bd2127fb65dc257dfebf08b611786227
Bram Vandenbogaerde <bram@vdb.space>
2023-04-03 22:25:29 +0200

    pair and vector lattices

--- src/main/scala/maf/values/typeclasses/PairLattice.scala
+++ src/main/scala/maf/values/typeclasses/PairLattice.scala
@@ -1,17 +1,49 @@
 package maf.values.typeclasses
 
+import maf.util.datastructures.ListOps.*
 import maf.values.Lattice
 import maf.util.*
 import cats.extensions.*
+import cats.syntax.all.*
+import maf.values.LatticeTopUndefined
+import scala.concurrent.ExecutionContext
 
 type PairLattice_[V] = [L] =>> PairLattice[L, V]
 trait PairLattice[L, V] extends Lattice[L]:
-  def cons(a: V, b: V): L
-  def car[M[_]: MonadError[Error]: MonadJoin](a: L): M[V]
-  def cdr[M[_]: MonadError[Error]: MonadJoin](a: L): M[V]
+    def cons(a: V, b: V): L
+    def car[M[_]: MonadError[Error]: MonadJoin](a: L): M[V]
+    def cdr[M[_]: MonadError[Error]: MonadJoin](a: L): M[V]
 
 object PairLattice:
-  def apply[L, V](using p: PairLattice[L, V]): PairLattice[L, V] = p
+    def apply[L, V](using p: PairLattice[L, V]): PairLattice[L, V] = p
 
-//
-// case class AbstractPair[L](car: L, cdr: L)
+given simplePairLattice[L: Lattice]: PairLattice[AbstractPair[L], L] with {
+    def show(t: AbstractPair[L]): String =
+        s"(${t.car} . ${t.cdr})"
+
+    def bottom: AbstractPair[L] =
+        AbstractPair(Lattice[L].bottom, Lattice[L].bottom)
+    override def isBottom(x: AbstractPair[L]): Boolean =
+        Lattice[L].isBottom(x.car) && Lattice[L].isBottom(x.cdr)
+
+    def eql[B: BoolLattice: GaloisFrom[Boolean]](
+        x: AbstractPair[L],
+        y: AbstractPair[L]
+      ): B = ???
+    def join(x: AbstractPair[L], y: => AbstractPair[L]): AbstractPair[L] =
+        AbstractPair(Lattice[L].join(x.car, y.car), Lattice[L].join(x.cdr, y.cdr))
+    def split(v: AbstractPair[L]): Set[AbstractPair[L]] =
+        Lattice[L]
+            .split(v.car)
+            .cartesian(Lattice[L].split(v.cdr))
+            .map(AbstractPair(_, _))
+            .toSet
+    def subsumes(x: AbstractPair[L], y: => AbstractPair[L]): Boolean =
+        Lattice[L].subsumes(x.car, y.car) && Lattice[L].subsumes(x.cdr, y.cdr)
+    def top: AbstractPair[L] = throw LatticeTopUndefined
+    def car[M[_]: MonadError[Error]: MonadJoin](a: AbstractPair[L]): M[L] =
+        a.car.pure
+    def cdr[M[_]: MonadError[Error]: MonadJoin](a: AbstractPair[L]): M[L] =
+        a.cdr.pure
+    def cons(a: L, b: L): AbstractPair[L] = AbstractPair(a, b)
+}
--- src/main/scala/maf/values/typeclasses/VectorLattice.scala
+++ src/main/scala/maf/values/typeclasses/VectorLattice.scala
@@ -1,51 +1,78 @@
 package maf.values.typeclasses
 
+import maf.util.datastructures.ListOps.*
 import maf.values.Lattice
 import maf.util.*
 import maf.values.typeclasses.syntax.*
 import cats.extensions.MonadError
 import cats.extensions.Errors.raiseError
 import cats.syntax.all.*
+import maf.values.LatticeTopUndefined
 
 type VectorLattice_[V, I] = [L] =>> VectorLattice[L, V, I]
 trait VectorLattice[L, V, I: IntLattice] extends Lattice[L]:
-  def vector[M[_]: MonadError[Error]: MonadJoin](size: I, init: V): M[L]
-  def vectorSet[M[_]: MonadError[Error]: MonadJoin](
-      vec: L,
-      pos: I,
-      vlu: V
-  ): M[L]
-  def vectorRef[M[_]: MonadError[Error]: MonadJoin](vec: L, pos: I): M[V]
-  def vectorLength[M[_]: MonadError[Error]: MonadJoin, I: IntLattice](
-      vec: L
-  ): M[I]
+    def vector[M[_]: MonadError[Error]: MonadJoin](size: I, init: V): M[L]
+    def vectorSet[M[_]: MonadError[Error]: MonadJoin](
+        vec: L,
+        pos: I,
+        vlu: V
+      ): M[L]
+    def vectorRef[M[_]: MonadError[Error]: MonadJoin](vec: L, pos: I): M[V]
+    def vectorLength[M[_]: MonadError[Error]: MonadJoin](
+        vec: L
+      ): M[I]
 
 import maf.values.domains.ConstantPropagation.*
 
 /** A very imprecise implementation of the vector lattice */
-// case class AbstractVector[L, I](elems: L, siz: I)
-// given [L: Lattice]: VectorLattice[AbstractVector[L, CP[Int]], L, CP[Int]] with {
-//   def vector[M[_]: MonadError[Error]: MonadJoin](
-//       size: CP[Int],
-//       init: L
-//   ): M[AbstractVector[L, I]] =
-//     AbstractVector(init, size).pure
-//   def vectorSet[M[_]: MonadError[Error]: MonadJoin](
-//       vec: AbstractVector[L, I],
-//       pos: CP[Int],
-//       vlu: L
-//   ): M[AbstractVector[L, I]] =
-//     vec.copy(elems = Lattice[L].join(vec.elems, vlu)).pure || raiseError(
-//       OutOfBoundsError
-//     )
-//
-//   def vectorRef[M[_]: MonadError[Error]: MonadJoin](
-//       vec: AbstractVector[L, CP[Int]],
-//       pos: CP[Int]
-//   ): M[L] =
-//     vec.elems.pure || raiseError(OutOfBoundsError)
-//
-//   def vectorLength[M[_]: MonadError[Error]: MonadJoin, I: IntLattice](
-//       vec: AbstractVector[L, I]
-//   ): M[I] = vec.siz.pure
-// }
+case class AbstractVector[L, I](elems: L, siz: I)
+given [L: Lattice, I: IntLattice]: VectorLattice[AbstractVector[L, I], L, I] with {
+    def show(t: AbstractVector[L, I]): String =
+        s"#(size: ${IntLattice[I].show(t.siz)}, elems: {${Lattice[L].show(t.elems)}})"
+
+    def bottom: AbstractVector[L, I] = AbstractVector(Lattice[L].bottom, IntLattice[I].bottom)
+    override def isBottom(x: AbstractVector[L, I]): Boolean =
+        Lattice[L].isBottom(x.elems) && IntLattice[I].isBottom(x.siz)
+
+    def eql[B: BoolLattice: GaloisFrom[Boolean]](x: AbstractVector[L, I], y: AbstractVector[L, I]): B = ???
+    def join(x: AbstractVector[L, I], y: => AbstractVector[L, I]): AbstractVector[L, I] =
+        AbstractVector(Lattice[L].join(x.elems, y.elems), Lattice[I].join(x.siz, y.siz))
+
+    def split(v: AbstractVector[L, I]): Set[AbstractVector[L, I]] =
+        Lattice[L].split(v.elems).cartesian(Lattice[I].split(v.siz)).map(AbstractVector(_, _)).toSet
+
+    def subsumes(x: AbstractVector[L, I], y: => AbstractVector[L, I]): Boolean =
+        Lattice[L].subsumes(x.elems, y.elems) && Lattice[I].subsumes(x.siz, y.siz)
+
+    def top: AbstractVector[L, I] = throw LatticeTopUndefined
+
+    def vector[M[_]: MonadError[Error]: MonadJoin](
+        size: I,
+        init: L
+      ): M[AbstractVector[L, I]] =
+        AbstractVector(init, size).pure
+    def vectorSet[M[_]: MonadError[Error]: MonadJoin](
+        vec: AbstractVector[L, I],
+        pos: I,
+        vlu: L
+      ): M[AbstractVector[L, I]] =
+        MonadJoin[M[_]].cond(IntLattice[I].lt(pos, vec.siz)) {
+            vec.copy(elems = Lattice[L].join(vec.elems, vlu)).pure
+        } {
+            raiseError(OutOfBoundsError)
+        }
+
+    def vectorRef[M[_]: MonadError[Error]: MonadJoin](
+        vec: AbstractVector[L, I],
+        pos: I
+      ): M[L] =
+        MonadJoin[M[_]].cond(IntLattice[I].lt(pos, vec.siz)) {
+            vec.elems.pure
+        } {
+            raiseError(OutOfBoundsError)
+        }
+
+    def vectorLength[M[_]: MonadError[Error]: MonadJoin](
+        vec: AbstractVector[L, I]
+      ): M[I] = vec.siz.pure
+}
