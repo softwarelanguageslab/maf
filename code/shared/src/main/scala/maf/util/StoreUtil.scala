b94acce9c4d6a4d4ee5a32d0c2a4b55f73954534
Jens Van der Plas <jevdplas@vub.be>
2023-07-13 13:14:49 +0200

    Better strings for stores

--- code/shared/src/main/scala/maf/util/StoreUtil.scala
+++ code/shared/src/main/scala/maf/util/StoreUtil.scala
@@ -6,7 +6,9 @@ object StoreUtil:
     private def crop(string: String, length: Option[Int]): String = if string.length <= length.getOrElse(Int.MaxValue) then string else string.take(length.getOrElse(Int.MaxValue)) + "..."
     def storeString[V](store: Map[Address, V], primitives: Boolean = false, crp: Option[Int] = None): String =
         val strings = store.map({ case (a, v) => s"${StringUtil.toWidth(crop(a.toString, crp), 50)}: $v" })
-        val filtered = if primitives then strings else strings.filterNot(_.toLowerCase.nn.startsWith("prm"))
-        val size = filtered.size
-        val infoString = "σ" * 150 + s"\nThe store contains $size addresses (primitive addresses ${if primitives then "included" else "excluded"}).\n"
-        filtered.toList.sorted.mkString(infoString, "\n", "\n" + "σ" * 150)
+        val contents = (if primitives then strings else strings.filterNot(_.toLowerCase.nn.startsWith("prm"))).toList.sorted
+        val size = contents.size
+        val longest = contents.map(_.length).foldLeft(0)(Math.max)
+        val infoString = "σ" * longest + s"\nThe store contains $size addresses (primitive addresses ${if primitives then "included" else "excluded"}).\n"
+        contents.mkString(infoString, "\n", "\n" + "σ" * longest)
+
