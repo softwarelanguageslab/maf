4d5e2e9e01bd1247f5ab51c98517760aefe25442
Bram Vandenbogaerde <bram@vdb.space>
2024-05-28 15:09:27 +0200

    added option to output all program names used for benchmarking according to their source

--- code/jvm/src/main/scala/maf/cli/experiments/domain/DomainSize.scala
+++ code/jvm/src/main/scala/maf/cli/experiments/domain/DomainSize.scala
@@ -2,36 +2,67 @@ package maf.cli.experiments
 package domain
 
 import scala.util.Try
-import maf.util.{Writer, Reader}
+import maf.util.{Reader, Writer}
 import maf.language.scheme.SchemeParser
 import maf.lattice.HMapInstrument
 import maf.bench.scheme.*
 import maf.util.benchmarks.Table
 
-object DomainSize:
+trait DomainSizePrograms:
     import SchemeBenchmarkPrograms.*
-  
+    val programsByCategory = Map("ad" -> ad, "gabriel" -> gabriel, "gambit" -> gambit, "scp1" -> scp1, "toplas98" -> toplas98, "wcr2019" -> WCR2019)
+    val programs = programsByCategory.flatMap(_._2).toList
+
+object DomainSize extends DomainSizePrograms:
+
     type AnalysisResult = Map[Int, Int]
 
-    val programs = (ad ++ gabriel ++ gambit ++ scp1 ++ toplas98 ++ WCR2019).toList
+    private def printUsage(): Unit =
+        val usage = """Usage: 
+        |   programs          Writes the program names to an output file in `results` called `programs`
+        |   benchmark         Runs the benchmarks and writes the output to a timestamped file called `output` 
+        |                     in the output oflder 
+        """.stripMargin
 
-    private def runAnalysis(programName: String): Option[AnalysisResult] = 
-      val source   = Reader.loadFile(programName)
-      val program  = SchemeParser.parseProgram(source)
-      val analysis = SchemeAnalyses.contextInsensitiveAnalysis(program)
-      HMapInstrument.reset()
-      Try(analysis.analyze()).toOption.map(_ => HMapInstrument.count)
+        print(usage)
 
+    private def runAnalysis(programName: String): Option[AnalysisResult] =
+        val source = Reader.loadFile(programName)
+        val program = SchemeParser.parseProgram(source)
+        val analysis = SchemeAnalyses.contextInsensitiveAnalysis(program)
+        HMapInstrument.reset()
+        Try(analysis.analyze()).toOption.map(_ => HMapInstrument.count)
 
-    def main(args: Array[String]): Unit =
-        val results = programs.map(runAnalysis) 
-        val csv = programs.zip(results).foldLeft(Table.empty.copy(default=Some(0))) { case (table, (program, result)) => 
-            result.map(_.foldLeft(table) { case (table, (size, count)) => 
+    /**
+     * Write the program names to an output file structured as an SCV. each row has a number of columns corresponding to the benchmark used in each
+     * source. This can then be processed for counting purposes, SLOC, etc.
+     */
+    private def printProgramNames(): Unit =
+        val w = Writer.openTimeStamped("results/programs.csv")
+        val programs = programsByCategory.map { case (k, v) => k + "," + v.toList.mkString(",") }.mkString("\n")
+        Writer.write(w, programs)
+        Writer.close(w)
 
-              table.add(program, size.toString, count) }).getOrElse(table)}.toCSVString()
+    private def runBenchmarks(): Unit =
+        val results = programs.map(runAnalysis)
+        val csv = programs
+            .zip(results)
+            .foldLeft(Table.empty.copy(default = Some(0))) { case (table, (program, result)) =>
+                result
+                    .map(_.foldLeft(table) { case (table, (size, count)) =>
+                        table.add(program, size.toString, count)
+                    })
+                    .getOrElse(table)
+            }
+            .toCSVString()
 
         val w = Writer.openTimeStamped("results/output.csv")
         Writer.write(w, csv)
         Writer.close(w)
 
-
+    def main(args: Array[String]): Unit =
+        val action = Try(args(0)).toOption
+        action match
+            case Some("programs")  => printProgramNames()
+            case Some("benchmark") => runBenchmarks()
+            case _                 => printUsage()
