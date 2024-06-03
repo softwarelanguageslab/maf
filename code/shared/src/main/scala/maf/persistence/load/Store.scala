8f48aefa46b5f6e3813ac08aa260658a7b480fe3
Merlijn <github.y62fx@simplelogin.com>
2024-06-03 07:39:49 +0200

    feat(load): Add more decodable lattices and seperate them into a trait

--- code/shared/src/main/scala/maf/persistence/load/Store.scala
+++ code/shared/src/main/scala/maf/persistence/load/Store.scala
@@ -16,6 +16,8 @@ import maf.language.scheme.lattices.SchemeLattice
 import maf.language.scheme.SchemeLambdaExp
 import maf.core.Address
 import maf.modular.scheme.modf.BaseSchemeModFSemanticsM
+import maf.modular.scheme.SchemeConstantPropagationDomain
+import maf.lattice.ConstantPropagation.L
 
 /**
  * The base trait for decoding [[AbstractDomain.Value values]].
@@ -59,29 +61,51 @@ trait LoadLattice[Expr <: Expression] extends Load[Expr]:
             else if reader.tryReadString("bottom") then ConstantPropagation.Bottom
             else return new ConstantPropagation.Constant[T](reader.read[T]())
 
+trait LoadModularSchemeDomainLattices extends Load[SchemeExp] with ModularSchemeDomain:
+    given stringLatticeDecoder: Decoder[S]
+    given booleanLatticeDecoder: Decoder[B]
+    given integerLatticeDecoder: Decoder[I]
+    given realLatticeDecoder: Decoder[R]
+    given charLatticeDecoder: Decoder[C]
+    given symbolLatticeDecoder: Decoder[Sym]
+
+trait LoadSchemeConstantPropagationDomain extends SchemeConstantPropagationDomain with LoadModularSchemeDomainLattices with LoadLattice[SchemeExp]:
+    override given stringLatticeDecoder: Decoder[S] = latticeDecoder[L, String].asInstanceOf[Decoder[S]]
+    override given booleanLatticeDecoder: Decoder[B] = latticeDecoder[L, Boolean].asInstanceOf[Decoder[B]]
+    override given integerLatticeDecoder: Decoder[I] = latticeDecoder[L, BigInt].asInstanceOf[Decoder[I]]
+    override given realLatticeDecoder: Decoder[R] = latticeDecoder[L, Double].asInstanceOf[Decoder[R]]
+    override given charLatticeDecoder: Decoder[C] = latticeDecoder[L, Char].asInstanceOf[Decoder[C]]
+    override given symbolLatticeDecoder: Decoder[Sym] = latticeDecoder[L, String].asInstanceOf[Decoder[Sym]]
+
 /**
  * Trait to decode [[ModularSchemeLattice modular scheme lattices]].
  *
  * Implementation of [[LoadModularDomain]].
  */
-trait LoadModularSchemeLattices
+trait LoadModularSchemeDomain
     extends LoadModularDomain
     with LoadAddr[SchemeExp]
     with LoadExpressions[SchemeExp]
     with BaseSchemeModFSemanticsM
     with LoadEnvironment[SchemeExp]
-    with LoadLattice[SchemeExp]:
-    type SchemeLattice = ModularSchemeLattice[?, ?, ?, ?, ?, ?, ?]
+    with LoadLattice[SchemeExp]
+    with LoadComponents[SchemeExp]
+    with LoadSchemeConstantPropagationDomain:
+    type LoadSchemeLattice = ModularSchemeLattice[?, S, B, I, R, C, Sym]
     override def hMapDecoders = super.hMapDecoders ++ Set(
-      ("int", summon[Decoder[(HMapKey, SchemeLattice#Int)]]),
-      ("boolean", summon[Decoder[(HMapKey, SchemeLattice#Bool)]]),
-      ("string", summon[Decoder[(HMapKey, SchemeLattice#Str)]]),
-      ("primitive", summon[Decoder[(HMapKey, SchemeLattice#Prim)]]),
-      ("closure", summon[Decoder[(HMapKey, SchemeLattice#Clo)]]),
-      ("pointer", summon[Decoder[(HMapKey, SchemeLattice#Pointer)]]),
-      ("symbol", summon[Decoder[(HMapKey, SchemeLattice#Symbol)]]),
-      ("cons", summon[Decoder[(HMapKey, SchemeLattice#Cons)]]),
-      ("vector", summon[Decoder[(HMapKey, SchemeLattice#Vec)]]),
+      ("int", summon[Decoder[(HMapKey, LoadSchemeLattice#Int)]]),
+      ("boolean", summon[Decoder[(HMapKey, LoadSchemeLattice#Bool)]]),
+      ("string", summon[Decoder[(HMapKey, LoadSchemeLattice#Str)]]),
+      ("real", summon[Decoder[(HMapKey, LoadSchemeLattice#Real)]]),
+      ("char", summon[Decoder[(HMapKey, LoadSchemeLattice#Char)]]),
+      ("inputPort", summon[Decoder[(HMapKey, LoadSchemeLattice#InputPort)]]),
+      ("kont", KDecoder),
+      ("primitive", summon[Decoder[(HMapKey, LoadSchemeLattice#Prim)]]),
+      ("closure", summon[Decoder[(HMapKey, LoadSchemeLattice#Clo)]]),
+      ("pointer", summon[Decoder[(HMapKey, LoadSchemeLattice#Pointer)]]),
+      ("symbol", summon[Decoder[(HMapKey, LoadSchemeLattice#Symbol)]]),
+      ("cons", summon[Decoder[(HMapKey, LoadSchemeLattice#Cons)]]),
+      ("vector", summon[Decoder[(HMapKey, LoadSchemeLattice#Vec)]]),
       ("nil", nilLatticeDecoder),
       ("void", summon[Decoder[(HMapKey, modularLattice.Void.type)]]),
       ("ERROR", errorDecoder)
@@ -93,28 +117,48 @@ trait LoadModularSchemeLattices
             System.err.nn.println("The lattice was not correctly encoded and had error: `" + error + "`, using `nil` instead.")
             return (modularLattice.NilT, modularLattice.Nil)
 
-    given Decoder[(HMapKey, SchemeLattice#Int)] with
-        override def read(reader: Reader): (HMapKey, SchemeLattice#Int) =
-            val lattice = reader.read[Lattice[BigInt]]()
-            return (modularLattice.IntT, new modularLattice.Int(lattice.asInstanceOf[LoadModularSchemeLattices.this.modularLatticeWrapper.I]))
+    given Decoder[(HMapKey, LoadSchemeLattice#Int)] with
+        override def read(reader: Reader): (HMapKey, LoadSchemeLattice#Int) =
+            val lattice = reader.read[I]()
+            return (modularLattice.IntT, new modularLattice.Int(lattice))
 
-    given Decoder[(HMapKey, SchemeLattice#Bool)] with
-        override def read(reader: Reader): (HMapKey, SchemeLattice#Bool) =
-            val lattice = reader.read[Lattice[Boolean]]()
-            return (modularLattice.BoolT, new modularLattice.Bool(lattice.asInstanceOf[LoadModularSchemeLattices.this.modularLatticeWrapper.B]))
+    given Decoder[(HMapKey, LoadSchemeLattice#Real)] with
+        override def read(reader: Reader): (HMapKey, LoadSchemeLattice#Real) =
+            val lattice = reader.read[R]()
+            return (modularLattice.RealT, new modularLattice.Real(lattice))
 
-    given Decoder[(HMapKey, SchemeLattice#Str)] with
-        override def read(reader: Reader): (HMapKey, SchemeLattice#Str) =
-            val lattice = reader.read[Lattice[String]]()
-            return (modularLattice.StrT, new modularLattice.Str(lattice.asInstanceOf[LoadModularSchemeLattices.this.modularLatticeWrapper.S]))
+    given Decoder[(HMapKey, LoadSchemeLattice#Bool)] with
+        override def read(reader: Reader): (HMapKey, LoadSchemeLattice#Bool) =
+            val lattice = reader.read[B]()
+            return (modularLattice.BoolT, new modularLattice.Bool(lattice))
 
-    given Decoder[(HMapKey, SchemeLattice#Symbol)] with
-        override def read(reader: Reader): (HMapKey, SchemeLattice#Symbol) =
-            val lattice = reader.read[Lattice[String]]()
-            return (modularLattice.SymbolT, new modularLattice.Symbol(lattice.asInstanceOf[LoadModularSchemeLattices.this.modularLatticeWrapper.Sym]))
+    private given KDecoder: Decoder[(HMapKey, LoadSchemeLattice#K)] with
+        override def read(reader: Reader): (HMapKey, LoadSchemeLattice#K) =
+            val lattice = reader.read[Set[Component]]().asInstanceOf[Set[LoadSchemeLattice#K]]
+            return (modularLattice.KontT, new modularLattice.Kont(lattice))
 
-    given Decoder[(HMapKey, SchemeLattice#Prim)] with
-        override def read(reader: Reader): (HMapKey, SchemeLattice#Prim) =
+    given Decoder[(HMapKey, LoadSchemeLattice#Char)] with
+        override def read(reader: Reader): (HMapKey, LoadSchemeLattice#Char) =
+            val lattice = reader.read[C]()
+            return (modularLattice.CharT, new modularLattice.Char(lattice))
+
+    given Decoder[(HMapKey, LoadSchemeLattice#InputPort)] with
+        override def read(reader: Reader): (HMapKey, LoadSchemeLattice#InputPort) =
+            val lattice = reader.read[LoadSchemeLattice#L]()
+            return (modularLattice.InputPortT, new modularLattice.InputPort(lattice))
+
+    given Decoder[(HMapKey, LoadSchemeLattice#Str)] with
+        override def read(reader: Reader): (HMapKey, LoadSchemeLattice#Str) =
+            val lattice = reader.read[S]()
+            return (modularLattice.StrT, new modularLattice.Str(lattice))
+
+    given Decoder[(HMapKey, LoadSchemeLattice#Symbol)] with
+        override def read(reader: Reader): (HMapKey, LoadSchemeLattice#Symbol) =
+            val lattice = reader.read[Sym]()
+            return (modularLattice.SymbolT, new modularLattice.Symbol(lattice))
+
+    given Decoder[(HMapKey, LoadSchemeLattice#Prim)] with
+        override def read(reader: Reader): (HMapKey, LoadSchemeLattice#Prim) =
             return (modularLattice.PrimT, new modularLattice.Prim(reader.read[Set[String]]()))
 
     given MapDecoder[(SchemeLambdaExp, Env)] with
@@ -125,8 +169,8 @@ trait LoadModularSchemeLattices
             reader.close()
             return (expression.value, address.value)
 
-    given ArrayDecoder[(HMapKey, SchemeLattice#Clo)]() with
-        override def read(reader: Reader): (HMapKey, SchemeLattice#Clo) =
+    given ArrayDecoder[(HMapKey, LoadSchemeLattice#Clo)]() with
+        override def read(reader: Reader): (HMapKey, LoadSchemeLattice#Clo) =
             reader.start()
             val closures = reader.readUntilBeforeBreak[Set[(SchemeLambdaExp, Env)]](Set[(SchemeLambdaExp, Env)](),
                                                                                     (closures) => closures + (reader.read[(SchemeLambdaExp, Env)]())
@@ -134,30 +178,30 @@ trait LoadModularSchemeLattices
             reader.close()
             return (modularLattice.CloT, new modularLattice.Clo(closures))
 
-    given ArrayDecoder[(HMapKey, SchemeLattice#Pointer)]() with
-        override def read(reader: Reader): (HMapKey, SchemeLattice#Pointer) =
+    given ArrayDecoder[(HMapKey, LoadSchemeLattice#Pointer)]() with
+        override def read(reader: Reader): (HMapKey, LoadSchemeLattice#Pointer) =
             reader.start()
             val pointers = reader.readUntilBeforeBreak[Set[Address]](Set(), (pointers) => pointers + (reader.read[Address]()))
             reader.close()
             return (modularLattice.PointerT, new modularLattice.Pointer(pointers))
 
-    given MapDecoder[(HMapKey, SchemeLattice#Cons)]() with
-        override def read(reader: Reader): (HMapKey, SchemeLattice#Cons) =
+    given MapDecoder[(HMapKey, LoadSchemeLattice#Cons)]() with
+        override def read(reader: Reader): (HMapKey, LoadSchemeLattice#Cons) =
             reader.start()
-            val car = reader.readMember[SchemeLattice#L]("car")
-            val cdr = reader.readMember[SchemeLattice#L]("cdr")
+            val car = reader.readMember[LoadSchemeLattice#L]("car")
+            val cdr = reader.readMember[LoadSchemeLattice#L]("cdr")
             reader.close()
             return (modularLattice.ConsT, new modularLattice.Cons(car.value, cdr.value))
 
-    given MapDecoder[(HMapKey, SchemeLattice#Vec)] with LoadMapToArray with
-        override def read(reader: Reader): (HMapKey, SchemeLattice#Vec) =
+    given MapDecoder[(HMapKey, LoadSchemeLattice#Vec)] with LoadMapToArray with
+        override def read(reader: Reader): (HMapKey, LoadSchemeLattice#Vec) =
             reader.start()
-            val size = reader.readMember[Lattice[BigInt]]("size").value.asInstanceOf[LoadModularSchemeLattices.this.modularLatticeWrapper.I]
+            val size = reader.readMember[I]("size").value
             val elements =
                 reader
-                    .readMember[Map[Lattice[BigInt], SchemeLattice#L]]("elements")
+                    .readMember[Map[I, LoadSchemeLattice#L]]("elements")
                     .value
-                    .asInstanceOf[Map[LoadModularSchemeLattices.this.modularLatticeWrapper.I, SchemeLattice#L]]
+                    .asInstanceOf[Map[LoadModularSchemeDomain.this.modularLatticeWrapper.I, LoadSchemeLattice#L]]
             reader.close()
             return (modularLattice.VecT, new modularLattice.Vec(size, elements))
 
@@ -175,7 +219,7 @@ trait LoadModularSchemeLattices
  * Base trait for decoding values as [[ModularSchemeLattice modular scheme lattices]], as defined in [[ModularSchemeDomain]].
  *
  * @note
- *   This trait gives the methods needed to decode values, but does not implement them yet, other traits like [[LoadModularSchemeLattices]] should be
+ *   This trait gives the methods needed to decode values, but does not implement them yet, other traits like [[LoadModularSchemeDomain]] should be
  *   mixed in for the implementation. The trait that should be mixed in depends on the kind of values that is used in your analysis.
  *
  * @tparam Expr
