f992aa7da8c4fd6c1ae59f6c374a9b4ed44536fd
Merlijn <github.y62fx@simplelogin.com>
2024-07-09 08:02:27 +0200

    refactor(save): Create separate trait for lattices

--- code/shared/src/main/scala/maf/persistence/save/Store.scala
+++ code/shared/src/main/scala/maf/persistence/save/Store.scala
@@ -20,6 +20,9 @@ import maf.core.Environment
 import maf.lattice.{ConcreteLattice, ConstantPropagation}
 import maf.lattice.Concrete
 import maf.save.save.SaveExpressions
+import maf.modular.scheme.ModularSchemeLatticeWrapper
+import maf.modular.scheme.SchemeConstantPropagationDomain
+import maf.lattice.ConstantPropagation.L
 
 /**
  * Base trait for encoding [[AbstractDomain.Value values]].
@@ -66,55 +69,73 @@ trait SaveLattice[Expr <: Expression] extends Save[Expr]:
                 case ConstantPropagation.Bottom      => writer.write("bottom")
             writer
 
+trait SaveModularSchemeDomainLattices extends Save[SchemeExp] with ModularSchemeDomain:
+    given stringLatticeEncoder: Encoder[S]
+    given booleanLatticeEncoder: Encoder[B]
+    given integerLatticeEncoder: Encoder[I]
+    given realLatticeEncoder: Encoder[R]
+    given charLatticeEncoder: Encoder[C]
+    given symbolLatticeEncoder: Encoder[Sym]
+
+trait SaveSchemeConstantPropagationDomain extends SchemeConstantPropagationDomain with SaveModularSchemeDomainLattices with SaveLattice[SchemeExp]:
+    override given stringLatticeEncoder: Encoder[S] = latticeEncoder[L, String].asInstanceOf[Encoder[S]]
+    override given booleanLatticeEncoder: Encoder[B] = latticeEncoder[L, Boolean].asInstanceOf[Encoder[B]]
+    override given integerLatticeEncoder: Encoder[I] = latticeEncoder[L, BigInt].asInstanceOf[Encoder[I]]
+    override given realLatticeEncoder: Encoder[R] = latticeEncoder[L, Double].asInstanceOf[Encoder[R]]
+    override given charLatticeEncoder: Encoder[C] = latticeEncoder[L, Char].asInstanceOf[Encoder[C]]
+    override given symbolLatticeEncoder: Encoder[Sym] = latticeEncoder[L, String].asInstanceOf[Encoder[Sym]]
+
 /**
  * Trait for encoding values as [[ModularSchemeLattice modular scheme lattices]], as defined in [[ModularSchemeDomain]].
  *
  * Implementation of [[SaveValue]].
  */
-trait SaveModularDomain
+trait SaveModularSchemeDomain
     extends SaveValue[SchemeExp]
     with ModularSchemeDomain
     with SaveAddr[SchemeExp]
     with SaveExpressions[SchemeExp]
     with SaveEnvironment[SchemeExp]
-    with SaveLattice[SchemeExp]:
+    with SaveComponents[SchemeExp]
+    with SaveModularSchemeDomainLattices:
     /** Generic modular scheme lattice that is used for typechecking of nested class inside of this. */
-    type SchemeLattice = ModularSchemeLattice[?, ?, ?, ?, ?, ?, ?]
+    type SaveSchemeLattice = ModularSchemeLattice[?, S, B, I, R, C, Sym]
+
     override given valueEncoder: ArrayEncoder[HMap] with
         override def write(writer: Writer, hmap: HMap): Writer =
             writer.start()
             hmap.contents.foreach((key, value) => writer.writeMember((key, value)))
             writer.close()
 
-    given MapEncoder[SchemeLattice#Clo]() with
-        override def write(writer: Writer, closure: SchemeLattice#Clo): Writer =
-            writer.writeArrayOpen(closure.closures.size)
+    given MapEncoder[SaveSchemeLattice#Clo]() with
+        override def write(writer: Writer, closure: SaveSchemeLattice#Clo): Writer =
+            writer.writeArrayStart()
             closure.closures.foreach((clo) =>
                 writer.start()
                 writer.writeMember("expression", clo._1.asInstanceOf[SchemeExp])
                 writer.writeMember("address", clo._2.asInstanceOf[Environment[Address]])
                 writer.close()
             )
-            writer.writeArrayClose()
+            writer.writeBreak()
 
-    given ArrayEncoder[SchemeLattice#Pointer]() with
-        override def write(writer: Writer, pointer: SchemeLattice#Pointer): Writer =
+    given ArrayEncoder[SaveSchemeLattice#Pointer]() with
+        override def write(writer: Writer, pointer: SaveSchemeLattice#Pointer): Writer =
             writer.start()
             pointer.ptrs.foreach(writer.write(_))
             writer.close()
 
-    given MapEncoder[SchemeLattice#Cons]() with
-        override def write(writer: Writer, cons: SchemeLattice#Cons): Writer =
+    given MapEncoder[SaveSchemeLattice#Cons]() with
+        override def write(writer: Writer, cons: SaveSchemeLattice#Cons): Writer =
             writer.start()
             writer.writeMember("car", cons.car)
             writer.writeMember("cdr", cons.cdr)
             writer.close()
 
-    given MapEncoder[SchemeLattice#Vec]() with SaveMapToArray with
-        override def write(writer: Writer, vec: SchemeLattice#Vec): Writer =
+    given MapEncoder[SaveSchemeLattice#Vec]() with SaveMapToArray with
+        override def write(writer: Writer, vec: SaveSchemeLattice#Vec): Writer =
             writer.start()
-            writer.writeMember("size", vec.size.asInstanceOf[Lattice[BigInt]])
-            writer.writeMember("elements", vec.elements.asInstanceOf[Map[Lattice[BigInt], SchemeLattice#L]])
+            writer.writeMember("size", vec.size)
+            writer.writeMember("elements", vec.elements)
             writer.close()
 
     given MapEncoder[(HMapKey, Any)] with
@@ -123,17 +144,21 @@ trait SaveModularDomain
             val (key, value) = hMapPair
 
             value match {
-                case int: SchemeLattice#Int         => writer.writeMember("int", int.i.asInstanceOf[Lattice[BigInt]])
-                case bool: SchemeLattice#Bool       => writer.writeMember("boolean", bool.b.asInstanceOf[Lattice[Boolean]])
-                case str: SchemeLattice#Str         => writer.writeMember("string", str.s.asInstanceOf[Lattice[String]])
-                case symbol: SchemeLattice#Symbol   => writer.writeMember("symbol", symbol.s.asInstanceOf[Lattice[String]])
-                case prim: SchemeLattice#Prim       => writer.writeMember("primitive", prim.prims)
-                case clo: SchemeLattice#Clo         => writer.writeMember("closure", clo)
-                case pointer: SchemeLattice#Pointer => writer.writeMember("pointer", pointer)
-                case cons: SchemeLattice#Cons       => writer.writeMember("cons", cons)
-                case vec: SchemeLattice#Vec         => writer.writeMember("vector", vec)
-                case modularLattice.Nil             => writer.writeMember("nil", "")
-                case modularLattice.Void            => writer.writeMember("void", "")
+                case int: SaveSchemeLattice#Int             => writer.writeMember("int", int.i)
+                case bool: SaveSchemeLattice#Bool           => writer.writeMember("boolean", bool.b)
+                case str: SaveSchemeLattice#Str             => writer.writeMember("string", str.s)
+                case char: SaveSchemeLattice#Char           => writer.writeMember("char", char.c)
+                case inputPort: SaveSchemeLattice#InputPort => writer.writeMember("inputPort", inputPort.id)
+                case real: SaveSchemeLattice#Real           => writer.writeMember("real", real.r)
+                case symbol: SaveSchemeLattice#Symbol       => writer.writeMember("symbol", symbol.s)
+                case prim: SaveSchemeLattice#Prim           => writer.writeMember("primitive", prim.prims)
+                case clo: SaveSchemeLattice#Clo             => writer.writeMember("closure", clo)
+                case pointer: SaveSchemeLattice#Pointer     => writer.writeMember("pointer", pointer)
+                case cons: SaveSchemeLattice#Cons           => writer.writeMember("cons", cons)
+                case vec: SaveSchemeLattice#Vec             => writer.writeMember("vector", vec)
+                case kont: SaveSchemeLattice#Kont           => writer.writeMember("kont", kont.k.asInstanceOf[Set[Component]])
+                case modularLattice.Nil                     => writer.writeMember("nil", "")
+                case modularLattice.Void                    => writer.writeMember("void", "")
                 case _ =>
                     System.err.nn.println("The lattice with type `" + key.getClass + "` could not be encoded")
                     writer.writeMember("ERROR", "Unknown type: " + key.getClass.toString())
