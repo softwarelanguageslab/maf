c70cc126a4f9ec366a6cbfda7f993bdd6eb0847b
sverbele <sverbele@soft.vub.ac.be>
2026-03-31 13:23:51 +0200

    ControlEvalM monad

--- code/jvm/src/main/scala/maf/cli/experiments/clients/dynamicSlicing/DynamicSlicer.scala
+++ code/jvm/src/main/scala/maf/cli/experiments/clients/dynamicSlicing/DynamicSlicer.scala
@@ -13,12 +13,76 @@ import maf.modular.scheme.modf.SchemeModFNoSensitivity
 import maf.modular.scheme.SchemeConstantPropagationDomain
 import maf.modular.worklist.FIFOWorklistAlgorithm
 import maf.language.symbolic.lattices.SymbolicSchemeConstantPropagationDomain
+import maf.modular.scheme.modf.TEvalM
+
+import maf.core._
+import maf.core.Monad.MonadSyntaxOps
+import maf.core.Position._
+import maf.language.scheme._
+import maf.modular.scheme.modf.SchemeModFComponent._
+import maf.util.benchmarks.Timeout
+import maf.cli.experiments.clients.dynamicSlicing.TControlEvalM.ControlEvalM
+import maf.cli.experiments.clients.dynamicSlicing.TControlEvalM.MonadControlEvalM
 
 case class DynamicNode(reachableStmts: Set[DynamicNode], // reachableStmts maps a node to the set of all statements that can be reached from the given node
                        exp: SchemeExp, // the expression that this node belongs to
                        descendants: Set[DynamicNode]) //descendants are the direct descendants of the node
-    
+
+// a version of EvalM that saves the last control node passed
+object TControlEvalM: 
+    case class ControlEvalM[+X](run: (Environment[Address], Option[DynamicNode]) => Option[X]):
+       def flatMap[Y](f: X => ControlEvalM[Y]): ControlEvalM[Y] = ControlEvalM((env, node) => run(env, node).flatMap(res => f(res).run(env, node)))
+       def map[Y](f: X => Y): ControlEvalM[Y] = ControlEvalM((env, node) => run(env, node).map(f)) 
+
+    trait MonadControlEvalM extends TEvalM[ControlEvalM]:
+        def map[X, Y](m: ControlEvalM[X])(f: X => Y): ControlEvalM[Y] = m.map(f)
+        def flatMap[X, Y](m: ControlEvalM[X])(f: X => ControlEvalM[Y]): ControlEvalM[Y] = m.flatMap(f)
+        def unit[X](x: X): ControlEvalM[X] = ControlEvalM((_, _) => Some(x))
+        def mzero[X]: ControlEvalM[X] = ControlEvalM((_, _) => None)
+        implicit class MonadicOps[X](xs: Iterable[X]):
+            def foldLeftM[Y](y: Y)(f: (Y, X) => ControlEvalM[Y]): ControlEvalM[Y] = xs match
+                case Nil     => unit(y)
+                case x :: xs => f(y, x).flatMap(acc => xs.foldLeftM(acc)(f))
+            def mapM[Y](f: X => ControlEvalM[Y]): ControlEvalM[List[Y]] = xs match
+                case Nil => unit(Nil)
+                case x :: xs =>
+                    for
+                        fx <- f(x)
+                        rest <- xs.mapM(f)
+                    yield fx :: rest
+            def mapM_(f: X => ControlEvalM[Unit]): ControlEvalM[Unit] = xs match
+                case Nil     => unit(())
+                case x :: xs => f(x).flatMap(_ => xs.mapM_(f))  
+        def getEnv: ControlEvalM[Environment[Address]] = ControlEvalM((env, _) => Some(env))
+        def withEnv[X](f: Environment[Address] => Environment[Address])(ev: => ControlEvalM[X]): ControlEvalM[X] = 
+            ControlEvalM((env, node) => ev.run(f(env), node))  
+        def merge[X: Lattice](x: ControlEvalM[X], y: ControlEvalM[X]): ControlEvalM[X] = ControlEvalM { (env, node) =>
+            (x.run(env, node), y.run(env, node)) match
+                case (None, yres)             => yres
+                case (xres, None)             => xres
+                case (Some(res1), Some(res2)) => Some(Lattice[X].join(res1, res2))
+        }
+        def fail[X](err: Error): ControlEvalM[X] = throw new Exception(err.toString)
+        // ADDED FOR THE DYNAMICNODE
+        def getControlNode: ControlEvalM[DynamicNode] = ControlEvalM((_, node) => node)
+        def pushControlNode[X](node: DynamicNode)(ev: => ControlEvalM[X]): ControlEvalM[X] = 
+            ControlEvalM((env, _) => ev.run(env, Some(node))) 
+        def pushControlNodeM[X](node: DynamicNode)(ev: ControlEvalM[X]): ControlEvalM[X] =
+            given Monad[ControlEvalM] = this 
+            for 
+                result <- pushControlNode(node) { ev } 
+            yield result
+
+
 trait DynamicSlicer extends BigStepModFSemanticsT:
+    import TControlEvalM.{*}
+
+    object ControlEvalM extends MonadControlEvalM
+
+    override type EvalM[X] = ControlEvalM[X] 
+    implicit val evalM = ControlEvalM 
+
+
     // defnNode maps a variable name to the node in the graph that last assigned a value to that variable
     var defnNode: Map[Identifier, DynamicNode] = Map.empty
     // predNode maps a control predicate statement to the node that corresponds to the last occurrence of
@@ -38,7 +102,10 @@ trait DynamicSlicer extends BigStepModFSemanticsT:
     override def intraAnalysis(cmp: Component): DynamicSlicerIntra 
 
     trait DynamicSlicerIntra extends IntraAnalysis with BigStepModFIntraT: 
-        override def eval(exp: SchemeExp): EvalM[Value] = 
+        import evalM._
+        override def eval(exp: SchemeExp): ControlEvalM[Value] = 
+            
+
             // D: the set of nodes that last assigned values to the variables used by the expression
             val D: List[Option[DynamicNode]] = exp.usedSet().map(defnNode.get)
             // C: the control predicate node of the statement
@@ -67,7 +134,7 @@ trait DynamicSlicer extends BigStepModFSemanticsT:
                         updateDefnNode(newNode)
                 // if there is no node yet, we make a new one
                 case None => 
-                    nodes = nodes + node  
+                    nodes = nodes + node
                     updateDefnNode(node)
 
             super.eval(exp)
@@ -80,11 +147,10 @@ object DynamicSlicer:
             with StandardSchemeModFComponents
             with SchemeModFSemanticsM
             with SchemeModFNoSensitivity
-            with BigStepModFSemantics
             with SymbolicSchemeConstantPropagationDomain
             with FIFOWorklistAlgorithm[SchemeExp]
             with DynamicSlicer:
 
-            class AnalysisIntra(cmp: Component) extends IntraAnalysis(cmp) with DynamicSlicerIntra with BigStepModFIntra
+            class AnalysisIntra(cmp: Component) extends IntraAnalysis(cmp) with DynamicSlicerIntra
             override def intraAnalysis(cmp: Component): AnalysisIntra =
                 new AnalysisIntra(cmp)
